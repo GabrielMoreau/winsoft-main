1b7b139834b5fe0867275fb24bdec54f36ead95a
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-02-04 12:02:07 +0100

    Disable auto update

--- notepadpp/install.bat
+++ notepadpp/install.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 
 REM Hide the Window
-
 "cmdow.exe" @ /hid
 
 
+REM General parameter
 SET softexe=npp.8.3.Installer.x64.exe
 SET softversion=8.3
 SET softpatch=1
@@ -12,11 +12,15 @@ SET softname=Notepad++
 SET regkey=Notepad++
 
 
+REM Silent install
 "%softexe%" /S
 
 
-REM Change Add and Remove values
+REM Disable auto update
+RENAME "C:\Program Files\Notepad++\updater" "updater_disable"
 
+
+REM Better reg uninstall key
  > tmp_install.reg ECHO Windows Registry Editor Version 5.00
 >> tmp_install.reg ECHO.
 >> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
