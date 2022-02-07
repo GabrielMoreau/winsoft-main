13a6c86604db512a20c58d9f4cdf62d3529df20c
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-02-07 13:07:38 +0100

    Add 7-Zip recipe

---
+++ 7zip/install.bat
@@ -0,0 +1,33 @@
+REM @ECHO OFF
+
+REM Hide the Window
+REM "cmdow.exe" @ /hid
+
+REM
+REM   7-Zip
+REM
+
+REM Name
+SET softname=7-Zip
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
+
+SET softversion=18.05
+SET softversionshort=1805
+SET softpatch=1
+SET process=7z.exe
+
+
+REM Kill running process
+taskkill /T /F /IM %process%
+
+
+REM Silent install
+msiexec /i "7z%softversionshort%-x64.msi" /quiet
