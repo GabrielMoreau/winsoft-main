3a6b32e1d6e45a5306e0a27615094c2ca00fb809
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-02-07 12:30:50 +0100

    Add putty def

---
+++ putty/README.md
@@ -0,0 +1,7 @@
+# PuTTY - SSH Client
+
+PuTTY is a free and open-source terminal emulator, serial console and network file transfer application.
+It supports several network protocols, including SCP, SSH, Telnet, rlogin, and raw socket connection.
+
+* Website : https://www.chiark.greenend.org.uk/~sgtatham/putty/
+* Wikipedia : https://en.wikipedia.org/wiki/PuTTY
---
+++ putty/install.bat
@@ -0,0 +1,28 @@
+REM @ECHO OFF
+
+REM Hide the Window
+REM "cmdow.exe" @ /hid
+
+REM
+REM   PuTTY
+REM
+
+REM Name
+SET softname=PuTTY
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
+
+SET softversion=0.70
+SET softpatch=2
+
+
+REM Silent install
+msiexec /i putty-64bit-%softversion%-installer.msi /q
+
