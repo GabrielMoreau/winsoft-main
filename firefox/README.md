70a4f06924e49cd323fb2b700a5b72d556f316b2
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-02-08 10:05:32 +0100

    Auto update firefox

--- firefox/README.md
+++ firefox/README.md
@@ -1,6 +1,9 @@
-# Mozilla Firefox
+# Mozilla Firefox - Web navigator
+
+Firefox is a free and open-source web browser developed by the Mozilla Foundation.
 
 * Website : https://www.mozilla.org
+* Wikipedia : https://en.wikipedia.org/wiki/Firefox
+
 * Download ESR : https://www.mozilla.org/fr/firefox/enterprise/#download
 * Policies templates : https://github.com/mozilla/policy-templates/releases
-* Wikipedia : https://fr.wikipedia.org/wiki/Mozilla_Firefox
--- firefox/install.bat
+++ firefox/install.bat
@@ -1,17 +1,30 @@
-@ECHO OFF
+REM @ECHO OFF
 
 REM Hide the Window
+REM "cmdow.exe" @ /hid
 
-"cmdow.exe" @ /hid
+REM
+REM   VLC
+REM
+
+REM Name
+SET softname=Mozilla Firefox
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
 
 SET softversion=91.5.1
 SET softpatch=1
-SET softname=Mozilla Firefox
 SET process=firefox.exe
 
 
 REM Kill the current process
-
 VER | FIND /I "10.0" > nul
 IF %ERRORLEVEL%==0 TASKKILL /T /F /IM %process%
 
@@ -19,12 +32,14 @@ VER | FIND /I "6.1" > nul
 IF %ERRORLEVEL%==0 TASKKILL /T /F /IM %process%
 
 
+REM Silent install
 REM "Firefox Setup %softversion%esr.exe" -ms
-msiexec /i "Firefox Setup %softversion%esr.msi" INSTALL_MAINTENANCE_SERVICE=false /q
+msiexec /i "Firefox_Setup_%softversion%esr.msi" INSTALL_MAINTENANCE_SERVICE=false /q
+
 
 REM voir https://github.com/mozilla/policy-templates/blob/master/README.md
 IF NOT EXIST "C:\Program Files\Mozilla Firefox\distribution" MKDIR "C:\Program Files\Mozilla Firefox\distribution"
-IF  EXIST "C:\Program Files\Mozilla Firefox\distribution" COPY /y policies.json "C:\Program Files\Mozilla Firefox\distribution\policies.json" >NUL
+IF EXIST "C:\Program Files\Mozilla Firefox\distribution" COPY /y policies.json "C:\Program Files\Mozilla Firefox\distribution\policies.json" >NUL
 
 
 REM IF EXIST "C:\Program Files (x86)\Mozilla Firefox\" COPY /y local-settings.js "C:\Program Files (x86)\Mozilla Firefox\defaults\pref" >NUL
@@ -32,6 +47,3 @@ REM IF EXIST "C:\Program Files (x86)\Mozilla Firefox\" COPY /y mozilla.cfg "C:\P
 
 REM IF EXIST "C:\Program Files\Mozilla Firefox\" COPY /y local-settings.js "C:\Program Files\Mozilla Firefox\defaults\pref" >NUL
 REM IF EXIST "C:\Program Files\Mozilla Firefox\" COPY /y mozilla.cfg "C:\Program Files\Mozilla Firefox\mozilla.cfg" >NUL
-
-
-EXIT
