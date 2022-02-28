328395755fe94fcf84a5a4d50aa8f5c8f965ac4f
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-02-28 13:16:58 +0100

    Add Zoom def

---
+++ zoom/README.md
@@ -0,0 +1,9 @@
+# Zoom - Web Conference
+
+Zoom Meetings is a proprietary video teleconferencing software program.
+
+* Website : https://zoom.us/
+* Wikipedia : https://en.wikipedia.org/wiki/Zoom_(software)
+
+* Download : https://zoom.us/download
+* Configuration : https://support.zoom.us/hc/fr/articles/201362163-Installation-et-configuration-de-masse-pour-Windows
---
+++ zoom/install.bat
@@ -0,0 +1,32 @@
+REM @ECHO OFF
+REM Hide the Window
+
+REM "cmdow.exe" @ /hid
+
+REM
+REM   Zoom
+REM
+
+REM Name
+SET softname=Zoom
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
+
+REM https://support.zoom.us/hc/fr/articles/201362163-Installation-et-configuration-de-masse-pour-Windows
+
+SET softversion=5.8.6
+SET softpatch=1
+
+
+REM Silent install
+msiexec /i ZoomInstallerFull-$(VERSION).msi /quiet /qn /norestart /log c:\temp\zoommsi.log MSIRESTARTMANAGERCONTROL=”Disable” ZoomAutoUpdate="true" ZNoDesktopShortCut="true" ZSSOHOST="univ-grenoble-alpes-fr.zoom.us" ZConfig="nogoogle=1;nofacebook=1" ZRecommend="AudioAutoAdjust=1" 
+
+
+EXIT
