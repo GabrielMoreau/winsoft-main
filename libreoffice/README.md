530563ae237dce19b626bee5d9802fb281fad151
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-03-02 20:25:42 +0100

    Add LibreOffice packaging

---
+++ libreoffice/README.md
@@ -0,0 +1,7 @@
+# LibreOffice - Office suite based on the OpenDocument standard
+
+LibreOffice is a free and open-source office productivity software suite.
+LibreOffice uses the OpenDocument standard as its native file format.
+
+* Website : https://www.libreoffice.org/
+* Wikipedia : https://en.wikipedia.org/wiki/LibreOffice
---
+++ libreoffice/install.bat
@@ -0,0 +1,34 @@
+REM Hide the Window
+REM "cmdow.exe" @ /hid
+
+REM
+REM   LibreOffice
+REM
+
+REM Name
+SET softname=LibreOffice
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
+
+SET softversion=7.1.3
+SET softpatch=1
+
+SET process=soffice.bin
+
+REM https://wiki.documentfoundation.org/Deployment_and_Migration
+
+REM Silent install
+msiexec /qn /i "LibreOffice_%softversion%_Win_x64.msi" /l "%logdir%\%softname%-exe.txt" CREATEDESKTOPLINK=0 RebootYesNo=No ISCHECKFORPRODUCTUPDATES=0 REGISTER_NO_MSO_TYPES=1 REMOVE=gm_o_Onlineupdate SELECT_WORD=0 SELECT_EXCEL=0 SELECT_POWERPOINT=0 USERNAME="LEGI" ADDLOCAL=ALL
+
+REM Silent help install
+msiexec /qn /i "LibreOffice_%softversion%_Win_x64_helppack_fr.msi" /l "%logdir%\%softname%-help.txt"
+
+
+EXIT
