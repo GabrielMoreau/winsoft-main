67ad24a40ad6b6145c193bd38098f68097febcac
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-06-01 13:25:21 +0200

    Add inkscape (not validate)

---
+++ inkscape/README.md
@@ -0,0 +1,10 @@
+# Inkscape - Vector graphics editor
+
+Inkscape is a free and open-source vector graphics editor used
+to create vector images,
+primarily in Scalable Vector Graphics (SVG) format.
+
+* Website : https://inkscape.org/
+* Wikipedia : https://fr.wikipedia.org/wiki/Inkscape
+
+* Download : https://inkscape.org/release/inkscape-dev/?latest=1
---
+++ inkscape/install.bat
@@ -0,0 +1,42 @@
+REM @ECHO OFF
+REM Hide the Window
+
+REM "cmdow.exe" @ /hid
+
+REM
+REM   Inkscape
+REM
+
+REM Name
+SET softname=Inkscape
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
+
+SET softversion=91.5.1
+SET softpatch=1
+REM SET regkey=Inkscape (%softversion%)
+
+
+REM Silent install
+msiexec /i "inkscape-%softversion%-x64.msi" ALLUSERS=1 /qn
+
+
+REM Change Add and Remove values in the register
+REM > tmp_install.reg ECHO Windows Registry Editor Version 5.00
+REM >> tmp_install.reg ECHO.
+REM >> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
+REM >> tmp_install.reg ECHO "DisplayVersion"="%softversion% (fr)"
+REM >> tmp_install.reg ECHO "Comments"="Package OCS v%softpatch% (%DATE:~-4%/%DATE:~-7,-5%/%DATE:~-10,-8%)"
+REM >> tmp_install.reg ECHO "DisplayName"="%softname% (%softversion% OCS)"
+REM >> tmp_install.reg ECHO.
+REM regedit.exe /S "tmp_install.reg"
+
+
+EXIT
