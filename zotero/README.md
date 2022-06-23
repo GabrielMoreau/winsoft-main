85b7729939493777b43e57f8a54b3cedc674cac4
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-06-23 11:07:42 +0200

    Add Zotero definition

---
+++ zotero/README.md
@@ -0,0 +1,9 @@
+# Zotero - Manage bibliographic data
+
+Zotero is a free and open-source reference management software to manage
+bibliographic data and related research materials.
+
+* Website : https://www.zotero.org/
+* Wikipedia : https://www.wikipedia.org/wiki/Zotero
+
+* Download : https://www.zotero.org/download/
---
+++ zotero/install.bat
@@ -0,0 +1,27 @@
+REM @ECHO OFF
+
+REM
+REM   Zotero
+REM
+
+REM Name
+SET softname=Zotero
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
+
+SET softversion=91.5.1
+SET softpatch=1
+
+
+REM Silent install
+Zotero-%softversion%_setup.exe /S
+
+
+EXIT
