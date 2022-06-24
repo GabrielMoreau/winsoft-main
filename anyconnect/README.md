bb7ff4d18e00e8b48b2f553b8b26943c251313db
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-06-24 12:39:03 +0200

    Add Cisco AnyConnect rules

---
+++ anyconnect/README.md
@@ -0,0 +1,14 @@
+# AnyConnect - Cisco AnyConnect VPN
+
+Cisco AnyConnect Secure Mobility Client is a proprietary VPN client
+for connecting to Cisco VPN concentrators.
+
+* Website : https://www.cisco.com/c/en/us/security/vpn-endpoint-security-clients/
+* Wikipedia : https://fr.wikipedia.org/wiki/Cisco_AnyConnect_Secure_Mobility_Client
+
+* Download : https://nomadisme.grenet.fr/
+
+Manually download the `anyconnect-win-XXXX-core-vpn-webdeploy-k9.msi` file
+in the current folder and remember to update the version number in the `Makefile`.
+
+For this installer, we have not found a way to automate this task.
---
+++ anyconnect/install.bat
@@ -0,0 +1,40 @@
+REM @ECHO OFF
+
+REM
+REM   AnyConnect
+REM
+
+REM Name
+SET softname=AnyConnect
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
+
+SET softversion=4.10.04071
+SET softpatch=1
+
+
+REM Silent install
+msiexec /i "anyconnect-win-%softversion%-core-vpn-webdeploy-k9.msi" ALLUSERS=1 /qn /norestart /L*v "%logdir%\%softname%-MSI.txt"
+
+
+REM Default profile
+IF NOT EXIST "%ProgramData%\Cisco" (
+  MKDIR "%ProgramData%\Cisco"
+)
+IF NOT EXIST "%ProgramData%\Cisco\Cisco AnyConnect Secure Mobility Client" (
+  MKDIR "%ProgramData%\Cisco\Cisco AnyConnect Secure Mobility Client"
+)
+IF NOT EXIST "%ProgramData%\Cisco\Cisco AnyConnect Secure Mobility Client\Profile" (
+  MKDIR "%ProgramData%\Cisco\Cisco AnyConnect Secure Mobility Client\Profile"
+)
+COPY /B /Y "Profile_VPN_Default.xml" "%ProgramData%\Cisco\Cisco AnyConnect Secure Mobility Client\Profile\Profile_VPN_Default.xml"
+
+
+EXIT
