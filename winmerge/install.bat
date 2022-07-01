d4135ae801689da2003b1c454a9c2c6e1f0e91cf
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-07-01 09:20:21 +0200

    Add comment from old script

--- winmerge/install.bat
+++ winmerge/install.bat
@@ -19,10 +19,26 @@ EXIT /B
 
 SET softversion=2.6.11
 SET softpatch=1
+REM SET regkey=WinMerge-%softversion%_is1
+REM SET process=WinMergeU.exe
+
+REM Kill the current process
+REM VER | FIND /I "XP" > nul
+REM IF %ERRORLEVEL%==0 TASKKILL /T /F /IM %process%
 
 
 REM Silent install
 WinMerge-%softversion%-x64-Setup.exe /VERYSILENT /NORESTART /LOG="%logdir%\%softname%-MSI.txt"
+REM "%softname%-%softversion%-Setup.exe" /VERYSILENT /SP- /NORESTART
 
+REM Change Add and Remove values in the register
+REM  > tmp_install.reg ECHO Windows Registry Editor Version 5.00
+REM >> tmp_install.reg ECHO.
+REM >> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
+REM >> tmp_install.reg ECHO "DisplayVersion"="%softversion% (fr)"
+REM >> tmp_install.reg ECHO "Comments"="Package OCS v%softpatch% (%DATE:~-4%/%DATE:~-7,-5%/%DATE:~-10,-8%)"
+REM >> tmp_install.reg ECHO "DisplayName"="%softname% (%softversion% OCS)"
+REM >> tmp_install.reg ECHO.
+REM regedit.exe /S "tmp_install.reg"
 
 EXIT
