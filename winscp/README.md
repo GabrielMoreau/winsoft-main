fd0a2fb2a58fa26c17d300d7fdfcd99b98b7e24d
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-07-01 18:54:57 +0200

    Add WinSCP copy program

---
+++ winscp/README.md
@@ -0,0 +1,11 @@
+# WinSCP - SCP and SFTP file transfert
+
+WinSCP is a free and open-source SSH File Transfer Protocol (SFTP),
+File Transfer Protocol (FTP), WebDAV, Amazon S3, and secure copy
+protocol (SCP) client for Microsoft Windows. 
+
+* Website : https://winscp.net/
+* Wikipedia : https://fr.wikipedia.org/wiki/WinSCP
+
+* Download : https://winscp.net/eng/download.php
+* Silent install : https://silentinstallhq.com/winscp-silent-install-how-to-guide/
---
+++ winscp/install.bat
@@ -0,0 +1,36 @@
+REM @ECHO OFF
+
+REM
+REM   WinSCP
+REM
+
+REM Name
+SET softname=WinSCP
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
+
+
+SET softversion=2.6.11
+SET softpatch=1
+
+
+REM Silent install
+WinSCP-%softversion%-Setup.exe /VERYSILENT /NORESTART /ALLUSERS /MERGETASKS=!desktopicon /LOG="%logdir%\%softname%-MSI.txt"
+
+REM Comment beacuse of HKCU reg
+REM Disable WinSCP Check For Updates
+REM REG ADD "HKCU\SOFTWARE\Martin Prikryl\WinSCP 2\Configuration\Interface\Updates" /v "Period" /t REG_DWORD /d "00000000" /f
+REM REG ADD "HKCU\SOFTWARE\Martin Prikryl\WinSCP 2\Configuration\Interface\Updates" /v "ShowOnStartup" /t REG_DWORD /d "00000000" /f
+REM Disable WinSCP Check For Beta Versions
+REM REG ADD "HKCU\SOFTWARE\Martin Prikryl\WinSCP 2\Configuration\Interface\Updates" /v "BetaVersions" /t REG_DWORD /d "00000001" /f
+REM Disable WinSCP Collection of Anonymous Usage Statistics
+REM REG ADD "HKCU\SOFTWARE\Martin Prikryl\WinSCP 2\Configuration\Interface" /v "CollectUsage" /t REG_DWORD /d "00000000" /f
+
+EXIT
