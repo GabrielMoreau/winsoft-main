69b52a0ab4f0c920ae0a7ececd712bb68121caa1
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-07-01 09:51:37 +0200

    Add TeamViewer rules

---
+++ teamviewer/README.md
@@ -0,0 +1,10 @@
+# TeamViewer - Remote access and remote control
+
+TeamViewer is a remote access and remote control computer software,
+allowing maintenance of computers and other devices.
+
+* Website : https://www.teamviewer.com
+* Wikipedia : https://fr.wikipedia.org/wiki/TeamViewer
+
+* Download : https://www.teamviewer.com/fr/telecharger/windows/
+* Silent install : https://silentinstallhq.com/teamviewer-silent-install-how-to-guide/
---
+++ teamviewer/install.bat
@@ -0,0 +1,34 @@
+REM @ECHO OFF
+
+REM
+REM   TeamViewer
+REM
+
+REM Name
+SET softname=TeamViewer
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+EXIT /B
+
+:INSTALL
+
+
+SET softversion=2.6.11
+SET softpatch=1
+
+
+REM Silent install
+REM TeamViewer_Setup.exe /S
+TeamViewer-%softversion%-Setup-x64.exe
+
+
+REM Disable auto update
+REG ADD "HKLM\SOFTWARE\WOW6432Node\TeamViewer" /v AutoUpdateMode /t REG_DWORD /d 3 /f
+REG ADD "HKLM\SOFTWARE\WOW6432Node\TeamViewer" /v UpdateCheckInterval /t REG_DWORD /d 2 /f
+cmd /c "net stop "TeamViewer" && net start "TeamViewer""
+
+EXIT
