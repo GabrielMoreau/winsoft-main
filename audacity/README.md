bb8e9061a8abbd46d9e0289a055c88e7f656597a
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-07-27 16:38:21 +0200

    Update CLI for silent install

--- audacity/README.md
+++ audacity/README.md
@@ -7,3 +7,4 @@ application software.
 * Wikipedia : https://en.wikipedia.org/wiki/Audacity_(audio_editor)
 
 * Download : https://www.audacityteam.org/download/windows/
+* Silent install : https://silent-install.net/software/audacity_team/audacity/2.3.0
--- audacity/install.bat
+++ audacity/install.bat
@@ -11,16 +11,20 @@ SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
 IF NOT EXIST "%logdir%" (
   MKDIR "%logdir%"
 )
-CALL :INSTALL 1> "%logdir%\%softname%.txt" 2>&1
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
 EXIT /B
 
 :INSTALL
+
+ECHO BEGIN %date%-%time%
 
 SET softversion=91.5.1
 SET softpatch=1
 
 REM Silent install
-audacity-win-%softversion%-64bit.exe /S
+audacity-win-%softversion%-64bit.exe /TASKS="!desktopicon,!resetprefs" /VERYSILENT /NORESTART /LOG="%logdir%\%softname%-MSI.log" 
 
+
+ECHO END %date%-%time%
 
 EXIT
