38b820eb6fddea41e32210d44c3c0838ba8f9d6f
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-10-19 10:46:48 +0200

    Add Nextcloud package

---
+++ nextcloud/README.md
@@ -0,0 +1,9 @@
+# Nextcloud - Cloud Suite
+
+Nextcloud  is a suite of client–server software for creating
+and using file hosting services.
+
+* Website : https://nextcloud.com/
+* Wikipedia : https://en.wikipedia.org/wiki/Nextcloud
+
+* Download : https://download.nextcloud.com/desktop/releases/Windows/
---
+++ nextcloud/install.bat
@@ -0,0 +1,30 @@
+REM @ECHO OFF
+
+REM
+REM   Nextcloud
+REM
+
+REM Name
+SET softname=Nextcloud
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+SET softversion=91.5.1
+SET softpatch=1
+
+
+REM Silent install
+msiexec /i Nextcloud-%softversion%-x64.msi REBOOT=ReallySuppress /qn
+
+
+ECHO END %date%-%time%
+EXIT
