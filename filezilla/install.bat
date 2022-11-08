2f88099219f467e4d236a93dd372a9e24761d9b3
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-11-08 09:21:05 +0100

    Update install.bat softname

--- filezilla/install.bat
+++ filezilla/install.bat
@@ -5,7 +5,7 @@ REM   Filezilla
 REM
 
 REM Name
-SET softname=FileZilla Client
+SET softname=FileZilla-Client
 
 SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
 IF NOT EXIST "%logdir%" (
@@ -22,7 +22,6 @@ REM Version parameter (auto update by Makefile)
 SET softversion=3.55.0
 SET softpatch=7
 SET softexe=FileZilla_%softversion%_win64-setup.exe
-SET regkey=FileZilla Client
 
 ECHO Uninstall previous version if exist
 IF EXIST "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" (
@@ -37,19 +36,6 @@ ECHO Silent install
 
 ECHO Disable welcome and check update
 COPY /A /Y "fzdefaults.xml" "%ProgramFiles%\FileZilla FTP Client\fzdefaults.xml"
-
-REM reg query "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%"
-REM IF %ERRORLEVEL% EQU 0 (
-REM   ECHO Better reg uninstall key
-REM    > tmp_install.reg ECHO Windows Registry Editor Version 5.00
-REM   >> tmp_install.reg ECHO.
-REM   >> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
-REM   >> tmp_install.reg ECHO "DisplayVersion"="%softversion%"
-REM   >> tmp_install.reg ECHO "Comments"="Package OCS v%softpatch% (%DATE:~-4%/%DATE:~-7,-5%/%DATE:~-10,-8%)"
-REM   >> tmp_install.reg ECHO "DisplayName"="%softname% (%softversion% OCS/%softpatch%)"
-REM   >> tmp_install.reg ECHO.
-REM   regedit.exe /S "tmp_install.reg"
-REM )
 
 
 ECHO END %date%-%time%
--- vlc/install.bat
+++ vlc/install.bat
@@ -5,7 +5,7 @@ REM   VLC
 REM
 
 REM Name
-SET softname=VideoLAN VLC media player
+SET softname=VideoLAN-VLC-Media-Player
 
 SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
 IF NOT EXIST "%logdir%" (
@@ -20,31 +20,10 @@ ECHO BEGIN %date%-%time%
 
 SET softversion=3.0.13
 SET softpatch=1
-SET regkey=VLC media player
-SET process=vlc.exe
-
-
-REM SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
-REM IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
 
 
 REM Silent install
 "vlc-%softversion%-win64.exe" /S /NCRC
-
-
-REM Better reg uninstall key
-REM reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%"
-REM IF %ERRORLEVEL% EQU 0 (
-REM   ECHO Better reg uninstall key
-REM    > tmp_install.reg ECHO Windows Registry Editor Version 5.00
-REM   >> tmp_install.reg ECHO.
-REM   >> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
-REM   >> tmp_install.reg ECHO "DisplayVersion"="%softversion% (fr)"
-REM   >> tmp_install.reg ECHO "Comments"="Package OCS v%softpatch% (%DATE:~-4%/%DATE:~-7,-5%/%DATE:~-10,-8%)"
-REM   >> tmp_install.reg ECHO "DisplayName"="%softname% (%softversion% OCS/%softpatch%)"
-REM   >> tmp_install.reg ECHO.
-REM   regedit.exe /S "tmp_install.reg"
-REM )
 
 
 ECHO END %date%-%time%
