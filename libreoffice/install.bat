0db67c38bf39e77f142f0e8c56d731da9ef65175
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-02-06 13:31:24 +0100

    Update and msi change name...

--- libreoffice/install.bat
+++ libreoffice/install.bat
@@ -26,10 +26,10 @@ SET process=soffice.bin
 REM https://wiki.documentfoundation.org/Deployment_and_Migration
 
 REM Silent install
-msiexec /qn /i "LibreOffice_%softversion%_Win_x64.msi" /norestart /l "%logdir%\%softname%-MSI.log" CREATEDESKTOPLINK=0 RebootYesNo=No ISCHECKFORPRODUCTUPDATES=0 REGISTER_NO_MSO_TYPES=1 REMOVE=gm_o_Onlineupdate SELECT_WORD=0 SELECT_EXCEL=0 SELECT_POWERPOINT=0 USERNAME="LEGI" ADDLOCAL=ALL
+msiexec /qn /i "LibreOffice_%softversion%_Win_x86-64.msi" /norestart /l "%logdir%\%softname%-MSI.log" CREATEDESKTOPLINK=0 RebootYesNo=No ISCHECKFORPRODUCTUPDATES=0 REGISTER_NO_MSO_TYPES=1 REMOVE=gm_o_Onlineupdate SELECT_WORD=0 SELECT_EXCEL=0 SELECT_POWERPOINT=0 USERNAME="LEGI" ADDLOCAL=ALL
 
 REM Silent help install
-msiexec /qn /i "LibreOffice_%softversion%_Win_x64_helppack_fr.msi" /norestart /l "%logdir%\%softname%-HELP.log"
+msiexec /qn /i "LibreOffice_%softversion%_Win_x86-64_helppack_fr.msi" /norestart /l "%logdir%\%softname%-HELP.log"
 
 
 ECHO END %date%-%time%
