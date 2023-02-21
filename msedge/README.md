3b64172b168c54b7534de408ad9426eb9dd78921
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-02-21 15:27:27 +0100

    Add MS Edge package

---
+++ msedge/README.md
@@ -0,0 +1,10 @@
+# MSEdge - Microsoft Edge (Chrome) navigator
+
+Microsoft Edge is a proprietary, cross-platform web browser
+built on a Chromium-based.
+
+* Website : https://www.microsoft.com/en-us/edge/business
+* Wikipedia : https://en.wikipedia.org/wiki/Microsoft_Edge
+
+* Download : https://www.microsoft.com/en-us/edge/business/download
+* Silent Install : https://silentinstallhq.com/microsoft-edge-version-101-silent-install-how-to-guide/
---
+++ msedge/install.bat
@@ -0,0 +1,30 @@
+REM @ECHO OFF
+
+REM
+REM   MSEdge
+REM
+
+REM Name
+SET softname=MSEdge
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+SET softversion=91.5.1
+SET softpatch=1
+
+
+REM Silent install
+msiexec /i MicrosoftEdgeEnterpriseX64-%softversion%.msi DONOTCREATEDESKTOPSHORTCUT=true DONOTCREATETASKBARSHORTCUT=true /qn /L*v "%logdir%\%softname%-MSI.log"
+
+
+ECHO END %date%-%time%
+EXIT
