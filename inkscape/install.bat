f71ae7de882ebe358d0c342ced2f3f8c7cefdf3d
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-02-27 12:53:26 +0100

    Just cleaner because I copy it to make new soft pkg

--- inkscape/install.bat
+++ inkscape/install.bat
@@ -20,22 +20,10 @@ ECHO BEGIN %date%-%time%
 
 SET softversion=91.5.1
 SET softpatch=1
-REM SET regkey=Inkscape (%softversion%)
 
 
-REM Silent install
+ECHO Silent install %softname%
 msiexec /i "inkscape-%softversion%-x64.msi" ALLUSERS=1 /qn
-
-
-REM Change Add and Remove values in the register
-REM > tmp_install.reg ECHO Windows Registry Editor Version 5.00
-REM >> tmp_install.reg ECHO.
-REM >> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
-REM >> tmp_install.reg ECHO "DisplayVersion"="%softversion% (fr)"
-REM >> tmp_install.reg ECHO "Comments"="Package OCS v%softpatch% (%DATE:~-4%/%DATE:~-7,-5%/%DATE:~-10,-8%)"
-REM >> tmp_install.reg ECHO "DisplayName"="%softname% (%softversion% OCS)"
-REM >> tmp_install.reg ECHO.
-REM regedit.exe /S "tmp_install.reg"
 
 
 ECHO END %date%-%time%
