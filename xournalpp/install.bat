3dcb974b8fb099c490540ce29556ec40db782c91
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-02-27 11:49:06 +0100

    Add XournalPP (++)

---
+++ xournalpp/install.bat
@@ -0,0 +1,30 @@
+REM @ECHO OFF
+
+REM
+REM   XournalPP
+REM
+
+REM Name
+SET softname=XournalPP
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+SET softversion=91.5.1
+SET softpatch=1
+
+
+REM Silent install
+xournalpp-%softversion%-windows.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /S /quiet
+
+
+ECHO END %date%-%time%
+EXIT
