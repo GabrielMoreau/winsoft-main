7e34fd8836019d6139c7958d13df7a820ec6a31e
De Marchi <olivier.de-marchi@grenoble-inp.fr>
2023-03-28 12:21:25 +0200

    add miktex

---
+++ miktex/install.bat
@@ -0,0 +1,36 @@
+REM @ECHO OFF
+
+REM
+REM   Mikex
+REM
+
+REM Name
+SET softname=Miktex
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+
+SET softversion=
+SET softpatch=1
+
+
+REM download
+miktexsetup_standalone.exe --verbose --local-package-repository=C:\temp\miktex --package-set=basic download
+
+REM Silent install
+miktexsetup_standalone.exe --verbose --local-package-repository=C:\temp\miktex --shared=yes --user-config="<APPDATA>\MiKTeX" --user-data="<LOCALAPPDATA>\MiKTeX" --user-install="<APPDATA>\MiKTeX" --package-set=basic install
+
+REM remove localdownload
+rmdir /S /Q "C:\temp\miktex"
+
+ECHO END %date%-%time%
+EXIT
