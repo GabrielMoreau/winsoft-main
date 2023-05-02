5b81c743bb845e7c6052510506030ec3305ddbd1
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-05-02 12:47:28 +0200

    Remove old version of 2015-2022 !

--- visualredist/post-install.ps1
+++ visualredist/post-install.ps1
@@ -1,6 +1,7 @@
 
 $Flag2022x86 = ''
 $Flag2022x64 = ''
+$RefName = 'Microsoft Visual C++ Redistributable'
 
 @(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
   Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
@@ -22,7 +23,7 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2008 (x64) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2008 (x64) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 			ElseIf ($DisplayName -match '2008.*x86') {
@@ -34,7 +35,7 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2008 (x86) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2008 (x86) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 
@@ -48,7 +49,7 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2010 (x64) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2010 (x64) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 			ElseIf ($DisplayName -match '2010.*x86') {
@@ -60,7 +61,7 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2010 (x86) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2010 (x86) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 
@@ -73,7 +74,7 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2012 (x64) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2012 (x64) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 			ElseIf ($DisplayName -match '2012.*x86') {
@@ -85,7 +86,7 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2012 (x86) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2012 (x86) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 
@@ -98,7 +99,7 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2013 (x64) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2013 (x64) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 			ElseIf ($DisplayName -match '2013.*x86') {
@@ -110,7 +111,7 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2013 (x86) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2013 (x86) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 
@@ -123,7 +124,7 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2015-2019 (x64) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2015-2019 (x64) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 			ElseIf ($DisplayName -match '(2015-2019|\s2015\s|\s2017\s).*x86') {
@@ -135,34 +136,62 @@ $Flag2022x64 = ''
 						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2015-2019 (x86) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2015-2019 (x86) redistributable already at version $DisplayVersion or higher"
 				}
 			}
 
 			ElseIf ($DisplayName -match '2015-2022.*x64') {
-				$Flag2022x64 = 'installed'
 				If ($DisplayVersion -lt [version]'__2015-2022-x64__') {
 					$Exe = '2015-2022\vc_redist.x64.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
 						Write-Output "Update Microsoft Visual C++ 2015-2022 (x64) redistributable"
-						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
+						$Proc = Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue' -PassThru
+						$Timeouted = $Null # Reset any previously set timeout
+						# Wait up to 180 seconds for normal termination
+						$Proc | Wait-Process -Timeout 300 -ErrorAction SilentlyContinue -ErrorVariable Timeouted
+						If ($Timeouted) {
+							# Terminate the process
+							$Proc | Kill
+							Write-Output "Error: kill $RefName vc_redist.x86 exe"
+							Return
+						} ElseIf ($Proc.ExitCode -ne 0) {
+							Write-Output "Error: $RefName uninstall return code $($Proc.ExitCode)"
+							Return
+						} Else {
+							$Flag2022x64 = 'installed'
+						}
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2015-2022 (x64) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2015-2022 (x64) redistributable already at version $DisplayVersion or higher"
+					$Flag2022x64 = 'installed'
 				}
 			}
 			ElseIf ($DisplayName -match '2015-2022.*x86') {
-				$Flag2022x86 = 'installed'
 				If ($DisplayVersion -lt [version]'__2015-2022-x86__') {
 					$Exe = '2015-2022\vc_redist.x86.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
 						Write-Output "Update Microsoft Visual C++ 2015-2022 (x86) redistributable"
-						Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue'
+						$Proc = Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue' -PassThru
+						$Timeouted = $Null # Reset any previously set timeout
+						# Wait up to 180 seconds for normal termination
+						$Proc | Wait-Process -Timeout 300 -ErrorAction SilentlyContinue -ErrorVariable Timeouted
+						If ($Timeouted) {
+							# Terminate the process
+							$Proc | Kill
+							Write-Output "Error: kill $RefName vc_redist.x86 exe"
+							Return
+						} ElseIf ($Proc.ExitCode -ne 0) {
+							Write-Output "Error: $RefName uninstall return code $($Proc.ExitCode)"
+							Return
+						} Else {
+							$Flag2022x86 = 'installed'
+						}
 					}
 				} Else {
-					Write-Output "Microsoft Visual C++ 2015-2022 (x86) redistributable already at version $DisplayVersion"
+					Write-Output "Microsoft Visual C++ 2015-2022 (x86) redistributable already at version $DisplayVersion or higher"
+					$Flag2022x86 = 'installed'
 				}
 			}
 		}
@@ -177,9 +206,13 @@ $Flag2022x64 = ''
 		$DisplayName  = $App.DisplayName
 		If (!($DisplayName -match 'Microsoft Visual .* Redistributable')) { Return }
 
-		If ((($DisplayName -match '(2015-2019|\s2015\s|\s2017\s).*x64') -And ($Flag2022x64 -eq 'installed')) -Or (($DisplayName -match '(2015-2019|\s2015\s|\s2017\s).*x86') -And ($Flag2022x86 -eq 'installed'))) {
+		If ((($DisplayName -match '(2015-2019|2015-2022|\s2015\s|\s2017\s).*x64') -And ($Flag2022x64 -eq 'installed')) -Or (($DisplayName -match '(2015-2019|2015-2022|\s2015\s|\s2017\s).*x86') -And ($Flag2022x86 -eq 'installed'))) {
 			$UninstallString = $App.UninstallString
 			If (!($UninstallString -match 'VC_redist.*.exe')) { Return }
+
+			[version]$DisplayVersion = $App.DisplayVersion
+			If (($DisplayName -match '2015-2022.*x64') -And (!($DisplayVersion -lt [version]'__2015-2022-x64__'))) { Return }
+			If (($DisplayName -match '2015-2022.*x86') -And (!($DisplayVersion -lt [version]'__2015-2022-x86__'))) { Return }
 
 			$UninstallSplit = $App.UninstallString -Split "exe"
 			$Exe = $UninstallSplit[0] + 'exe"'
