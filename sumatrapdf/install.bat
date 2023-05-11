e31f609ed3d41416e9e4e4dd19e4cd89431fb798
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-05-11 12:04:08 +0200

    Add post-install script to clean Sumatra Key

--- sumatrapdf/install.bat
+++ sumatrapdf/install.bat
@@ -30,5 +30,19 @@ IF EXIST "%ALLUSERSPROFILE%\Desktop\SumatraPDF.lnk" DEL /F /Q "%ALLUSERSPROFILE%
 IF EXIST "%ALLUSERSPROFILE%\Bureau\SumatraPDF.lnk"  DEL /F /Q "%ALLUSERSPROFILE%\Bureau\SumatraPDF.lnk"
 
 
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO unblock
+%pwrsh% "Unblock-File -Path .\*.ps1"
+
+ECHO Execute post-install script
+%pwrsh% -File ".\post-install.ps1"
+
+
 ECHO END %date%-%time%
 EXIT
---
+++ sumatrapdf/post-install.ps1
@@ -0,0 +1,54 @@
+
+# Clean old duplicate key with SumatraPDF in the name (same uninstall string)
+
+$RefVersion = '91.5.1'
+$RefUninstallString = ''
+$RefName = 'SumatraPDF'
+
+Function ToVersion {
+	Param (
+		[Parameter(Mandatory = $true)] [string]$Version
+	)
+
+	$Version = $Version -Replace '[^\d\.].*$', ''
+	$Version = $Version -Replace '\.00?$', ''
+	$Version = $Version -Replace '\.00?$', ''
+	Return [version]$Version
+}
+
+@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+	ForEach {
+		$App = (Get-ItemProperty -Path $_.PSPath)
+		$DisplayName = $App.DisplayName
+		If (!($DisplayName -match $RefName)) { Return }
+		$DisplayVersion = $App.DisplayVersion
+		$Exe = $App.UninstallString
+		$KeyPath = $App.PSPath
+		If ((ToVersion($DisplayVersion)) -eq (ToVersion($RefVersion))) {
+			$RefUninstallString = $Exe
+			Echo "Ref Key $DisplayName / $DisplayVersion / $Exe / $KeyPath"
+		} Else {
+			Echo "Other Key $DisplayName / $DisplayVersion / $Exe / $KeyPath"
+		}
+	}
+
+If ($RefUninstallString -ne '') {
+	@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+	  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+		ForEach {
+			$App = (Get-ItemProperty -Path $_.PSPath)
+			$DisplayName = $App.DisplayName
+			If (!($DisplayName -match $RefName)) { Return }
+			$DisplayVersion = $App.DisplayVersion
+			$Exe = $App.UninstallString
+			$KeyPath = $App.PSPath
+			# Echo "Check Key $DisplayName : $DisplayVersion < $RefVersion ?"
+			If ((($Exe -eq $RefUninstallString) -Or ($Exe -eq $Null)) -And ((ToVersion($DisplayVersion)) -lt (ToVersion($RefVersion)))) {
+				Echo "Remove Key $DisplayName / $DisplayVersion / $Exe / $KeyPath"
+				Remove-Item -Path "$KeyPath" -Force -Recurse -ErrorAction SilentlyContinue
+			} Else {
+				Echo "Keep Key $DisplayName / $DisplayVersion / $Exe / $KeyPath / $(ToVersion($DisplayVersion))"
+			}
+		}
+}
