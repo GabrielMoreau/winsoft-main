2417e1504dd3fbe8bb2dea6f119e79bd5c752615
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-05-22 14:17:30 +0200

    Better code (I wish)

--- visualredist/post-install.ps1
+++ visualredist/post-install.ps1
@@ -3,6 +3,18 @@ $Flag2022x86 = ''
 $Flag2022x64 = ''
 $RefName = 'Microsoft Visual C++ Redistributable'
 
+Function ToVersion {
+	Param (
+		[Parameter(Mandatory = $true)] [string]$Version
+	)
+
+	$Version = $Version -Replace '[^\d\.].*$', ''
+	$Version = $Version -Replace '\.00?$', ''
+	$Version = $Version -Replace '\.00?$', ''
+	Return [version]$Version
+}
+
+# Install last version
 @(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
   Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
 	ForEach {
@@ -10,12 +22,12 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 		$App = (Get-ItemProperty -Path $Key.PSPath)
 		$DisplayName  = $App.DisplayName
 		If ($DisplayName -match 'Microsoft Visual .* Redistributable') {
-			[version]$DisplayVersion = $App.DisplayVersion
+			$DisplayVersion = $App.DisplayVersion
 			$KeyProduct = $Key | Split-Path -Leaf
-			#Write-Output "# $DisplayName / $DisplayVersion / $KeyProduct"
+			Write-Output "Info: $DisplayName / $DisplayVersion / $KeyProduct"
 
 			If ($DisplayName -match '2008.*x64') {
-				If ($DisplayVersion -lt [version]'__2008-x64__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2008-x64__'))) {
 					$Exe = '2008\vcredist_x64.exe'
 					$Args = '/q'
 					If (Test-Path -Path "$Exe") {
@@ -27,7 +39,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 				}
 			}
 			ElseIf ($DisplayName -match '2008.*x86') {
-				If ($DisplayVersion -lt [version]'__2008-x64__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2008-x64__'))) {
 					$Exe = '2008\vcredist_x86.exe'
 					$Args = '/q'
 					If (Test-Path -Path "$Exe") {
@@ -41,7 +53,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 
 			ElseIf ($DisplayName -match '2010.*x64') {
 				# 10.0.40219.325
-				If ($DisplayVersion -lt [version]'__2010-x64__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2010-x64__'))) {
 					$Exe = '2010\vcredist_x64.exe'
 					$Args = '/q /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -53,7 +65,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 				}
 			}
 			ElseIf ($DisplayName -match '2010.*x86') {
-				If ($DisplayVersion -lt [version]'__2010-x86__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2010-x86__'))) {
 					$Exe = '2010\vcredist_x86.exe'
 					$Args = '/q /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -66,7 +78,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 			}
 
 			ElseIf ($DisplayName -match '2012.*x64') {
-				If ($DisplayVersion -lt [version]'__2012-x64__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2012-x64__'))) {
 					$Exe = '2012\vcredist_x64.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -78,7 +90,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 				}
 			}
 			ElseIf ($DisplayName -match '2012.*x86') {
-				If ($DisplayVersion -lt [version]'__2012-x86__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2012-x86__'))) {
 					$Exe = '2012\vcredist_x86.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -91,7 +103,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 			}
 
 			ElseIf ($DisplayName -match '2013.*x64') {
-				If ($DisplayVersion -lt [version]'__2013-x64__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2013-x64__'))) {
 					$Exe = '2013\vcredist_x64.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -103,7 +115,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 				}
 			}
 			ElseIf ($DisplayName -match '2013.*x86') {
-				If ($DisplayVersion -lt [version]'__2013-x86__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2013-x86__'))) {
 					$Exe = '2013\vcredist_x86.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -116,7 +128,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 			}
 
 			ElseIf ($DisplayName -match '(2015-2019|\s2015\s|\s2017\s).*x64') {
-				If ($DisplayVersion -lt [version]'__2015-2019-x64__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2015-2019-x64__'))) {
 					$Exe = '2015-2019\vc_redist.x64.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -128,7 +140,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 				}
 			}
 			ElseIf ($DisplayName -match '(2015-2019|\s2015\s|\s2017\s).*x86') {
-				If ($DisplayVersion -lt [version]'__2015-2019-x64__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2015-2019-x64__'))) {
 					$Exe = '2015-2019\vc_redist.x86.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -141,7 +153,7 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 			}
 
 			ElseIf ($DisplayName -match '2015-2022.*x64') {
-				If ($DisplayVersion -lt [version]'__2015-2022-x64__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2015-2022-x64__'))) {
 					$Exe = '2015-2022\vc_redist.x64.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -156,19 +168,17 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 							Write-Output "Error: kill $RefName vc_redist.x86 exe"
 							Return
 						} ElseIf ($Proc.ExitCode -ne 0) {
-							Write-Output "Error: $RefName uninstall return code $($Proc.ExitCode)"
+							Write-Output "Error: $RefName install return code $($Proc.ExitCode)"
 							Return
-						} Else {
-							$Flag2022x64 = 'installed'
 						}
 					}
 				} Else {
 					Write-Output "Microsoft Visual C++ 2015-2022 (x64) redistributable already at version $DisplayVersion or higher"
-					$Flag2022x64 = 'installed'
 				}
 			}
+
 			ElseIf ($DisplayName -match '2015-2022.*x86') {
-				If ($DisplayVersion -lt [version]'__2015-2022-x86__') {
+				If ((ToVersion($DisplayVersion)) -lt (ToVersion('__2015-2022-x86__'))) {
 					$Exe = '2015-2022\vc_redist.x86.exe'
 					$Args = '/install /quiet /norestart'
 					If (Test-Path -Path "$Exe") {
@@ -183,21 +193,36 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 							Write-Output "Error: kill $RefName vc_redist.x86 exe"
 							Return
 						} ElseIf ($Proc.ExitCode -ne 0) {
-							Write-Output "Error: $RefName uninstall return code $($Proc.ExitCode)"
+							Write-Output "Error: $RefName install return code $($Proc.ExitCode)"
 							Return
-						} Else {
-							$Flag2022x86 = 'installed'
 						}
 					}
 				} Else {
 					Write-Output "Microsoft Visual C++ 2015-2022 (x86) redistributable already at version $DisplayVersion or higher"
-					$Flag2022x86 = 'installed'
 				}
 			}
 		}
 	}
 
-# Clean old version
+# View all version and set flag
+@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+	ForEach {
+		$Key = $_
+		$App = (Get-ItemProperty -Path $Key.PSPath)
+		$DisplayName  = $App.DisplayName
+		If (!($DisplayName -match 'Microsoft Visual .* Redistributable')) { Return }
+
+		$DisplayVersion = $App.DisplayVersion
+		$KeyProduct = $Key | Split-Path -Leaf
+		$Exe = $App.UninstallString
+		"# {0,-66} / {1,-14} / {2} / {3}" -F $DisplayName, $DisplayVersion, $KeyProduct, $Exe
+
+		If (($DisplayName -match '2015-2022.*x86') -And ((ToVersion($DisplayVersion)) -ge (ToVersion('__2015-2022-x86__')))) { $Flag2022x86 = 'installed' }
+		If (($DisplayName -match '2015-2022.*x64') -And ((ToVersion($DisplayVersion)) -ge (ToVersion('__2015-2022-x64__')))) { $Flag2022x64 = 'installed' }
+	} | Sort-Object
+
+# Uninstall old version
 @(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
   Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
 	ForEach {
@@ -207,12 +232,17 @@ $RefName = 'Microsoft Visual C++ Redistributable'
 		If (!($DisplayName -match 'Microsoft Visual .* Redistributable')) { Return }
 
 		If ((($DisplayName -match '(2015-2019|2015-2022|\s2015\s|\s2017\s).*x64') -And ($Flag2022x64 -eq 'installed')) -Or (($DisplayName -match '(2015-2019|2015-2022|\s2015\s|\s2017\s).*x86') -And ($Flag2022x86 -eq 'installed'))) {
+			$DisplayVersion  = $App.DisplayVersion
 			$UninstallString = $App.UninstallString
-			If (!($UninstallString -match 'VC_redist.*.exe')) { Return }
 
-			[version]$DisplayVersion = $App.DisplayVersion
-			If (($DisplayName -match '2015-2022.*x64') -And (!($DisplayVersion -lt [version]'__2015-2022-x64__'))) { Return }
-			If (($DisplayName -match '2015-2022.*x86') -And (!($DisplayVersion -lt [version]'__2015-2022-x86__'))) { Return }
+			If (($DisplayName -match '2015-2022.*x64') -And ((ToVersion($DisplayVersion)) -ge (ToVersion('__2015-2022-x64__')))) { Return }
+			If (($DisplayName -match '2015-2022.*x86') -And ((ToVersion($DisplayVersion)) -ge (ToVersion('__2015-2022-x86__')))) { Return }
+
+			If (!($UninstallString -match 'VC_redist.*.exe')) {
+				$KeyProduct = $Key | Split-Path -Leaf
+				Write-Output "Error: HowTo Remove $DisplayName / $DisplayVersion / $KeyProduct / $UninstallString"
+				Return
+			}
 
 			$UninstallSplit = $App.UninstallString -Split "exe"
 			$Exe = $UninstallSplit[0] + 'exe"'
