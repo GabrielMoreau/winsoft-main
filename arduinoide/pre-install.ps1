c3ab6fdeb0bfea7c7b15dfcf3b2b977c3fd3b9cd
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-06-09 10:49:31 +0200

    Add cert to trust

---
+++ arduinoide/pre-install.ps1
@@ -0,0 +1,3 @@
+
+Import-Certificate -FilePath 'Adafruit.cer' -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+Import-Certificate -FilePath 'Arduino.cer'  -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
