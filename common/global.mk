89a9afbed3839495953dae0e6547156cd4718b64
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-06-13 09:55:13 +0200

    Give default parameter example

--- common/global.mk
+++ common/global.mk
@@ -1,2 +1,19 @@
 
+# List of global parameter
+# You can change it via the sinclude directive
+# do not change this file
+
+# Team Name (all package)
+IT_TEAM:=IT Team
+
+
+# OCS Inventory Agent (ocsinventory-agent package)
+OCS_SERVER:=https://ocs-server.example.com/ocsinventory
+OCS_SSL:=1
+
+# Cisco AnyConnect (anyconnect package)
+CISCO_VPN_SERVER:=vpn.example.com
+
+
+# Include your local parameter if exists
 sinclude ../../winsoft-conf/common/conf.mk
