8da9d741893293a84c12258e427c36d173256c81
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-06-29 13:48:30 +0200

    Add VMware Horizon Client

---
+++ vmwarehorizon/README.md
@@ -0,0 +1,11 @@
+# VMware Horizon Client - Client for VDI VMware Horizon (virtual desktop)
+
+VMware Horizon Clients allow you to connect to your VMware Horizon
+virtual desktop from your device of choice giving you on-the-go access
+from any location.
+
+* Website : https://www.vmware.com/products/horizon.html
+* Wikipedia : https://fr.wikipedia.org/wiki/VMwareHorizon
+
+* Download : https://en.wikipedia.org/wiki/VMware_Horizon
+* Silent install : https://silentinstallhq.com/vmware-horizon-client-silent-install-how-to-guide/
---
+++ vmwarehorizon/install.bat
@@ -0,0 +1,30 @@
+REM @ECHO OFF
+
+REM
+REM   VMwareHorizon
+REM
+
+REM Name
+SET softname=VMwareHorizon
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+SET softversion=__VERSIONLONG__
+SET softpatch=__PATCH__
+
+
+ECHO Silent install %softname%
+VMware-Horizon-Client-%softversion%.exe /silent /install /norestart /log "%logdir%\%softname%-MSI.log" DESKTOP_SHORTCUT=0
+
+
+ECHO END %date%-%time%
+EXIT
