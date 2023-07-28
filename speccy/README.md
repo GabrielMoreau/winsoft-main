c803f5a3624496b7df257f5e6c01dfd9b033714a
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-07-28 11:59:20 +0200

    Add Speccy

---
+++ speccy/README.md
@@ -0,0 +1,15 @@
+# Speccy - Display hardware computer information
+
+Speccy shows the user information about hardware and software of the
+computer. The information displayed by Speccy includes processor brand
+and model, hard drive size and speed, amount of memory (RAM),
+information about graphics card and operating system.
+
+* Website : https://www.ccleaner.com/speccy
+* Wikipedia : https://en.wikipedia.org/wiki/Speccy
+
+Be carreful, Speccy Free and Professional are for home use only.
+Use only on personnal computer.
+
+* Download : https://www.ccleaner.com/fr-fr/speccy/download
+* Silent install : https://silentinstallhq.com/speccy-silent-install-how-to-guide/
---
+++ speccy/install.bat
@@ -0,0 +1,35 @@
+REM @ECHO OFF
+
+REM
+REM   Speccy
+REM
+
+REM Name
+SET softname=Speccy
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+
+SET softversion=__VERSION__
+SET softpatch=__PATCH__
+
+
+ECHO Silent install %softname%
+spsetup%softversion%.exe /S
+
+ECHO Remove desktop shortcut
+IF EXIST "%PUBLIC%\Desktop\Speccy.lnk"          DEL /F /Q "%PUBLIC%\Desktop\Speccy.lnk"
+IF EXIST "%ALLUSERSPROFILE%\Desktop\Speccy.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\Speccy.lnk"
+
+ 
+ECHO END %date%-%time%
+EXIT
