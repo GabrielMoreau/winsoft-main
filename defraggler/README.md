3a5ef55a41ea6277aeefd29cfcf646a5f1b9637b
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-07-28 11:58:33 +0200

    Add Defraggler

---
+++ defraggler/README.md
@@ -0,0 +1,13 @@
+# Defraggler - Hard disk defragmentation utility
+
+Defraggler is a defragmentation utility that can defragment individual
+files or groups of files on computer system. 
+
+* Website : https://www.ccleaner.com/defraggler
+* Wikipedia : https://en.wikipedia.org/wiki/Defraggler
+
+Be carreful, Defraggler Free and Professional are for home use only.
+Use only on personnal computer.
+
+* Download : https://www.ccleaner.com/fr-fr/defraggler/download
+* Silent install : https://silentinstallhq.com/defraggler-silent-install-how-to-guide/
---
+++ defraggler/install.bat
@@ -0,0 +1,35 @@
+REM @ECHO OFF
+
+REM
+REM   Defraggler
+REM
+
+REM Name
+SET softname=Defraggler
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+
+SET softversion=__VERSION__
+SET softpatch=__PATCH__
+
+
+ECHO Silent install %softname%
+dfsetup%softversion%.exe /S
+
+ECHO Remove desktop shortcut
+IF EXIST "%PUBLIC%\Desktop\Defraggler.lnk"          DEL /F /Q "%PUBLIC%\Desktop\Defraggler.lnk"
+IF EXIST "%ALLUSERSPROFILE%\Desktop\Defraggler.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\Defraggler.lnk"
+
+ 
+ECHO END %date%-%time%
+EXIT
