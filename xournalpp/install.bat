99d5b2e6d982556daad8174feb9212c75302b880
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-07-30 14:26:54 +0200

    Add Xournal++ in menu and a registry key (we install under SYSTEM accoun)

--- xournalpp/install.bat
+++ xournalpp/install.bat
@@ -20,10 +20,42 @@ ECHO BEGIN %date%-%time%
 
 SET softversion=91.5.1
 SET softpatch=1
+SET regkey=Xournal++
+SET shortcut=%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\Xournal++.lnk
 
 
 ECHO Silent install %softname%
 xournalpp-%softversion%-windows.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /S /quiet
+
+ECHO Copy uninstall script
+COPY /A /Y "uninstall.bat" "%ProgramFiles%\%regkey%\uninstall.bat"
+
+
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Create shortcut
+IF EXIST "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs" (
+  %pwrsh% -Command "$WS = New-Object -ComObject WScript.Shell; $SC = $WS.CreateShortcut('%shortcut%'); $SC.TargetPath = '%ProgramFiles%\%regkey%\bin\xournalpp.exe'; $SC.Save();" -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
+)
+
+
+ECHO Better reg uninstall key
+ > tmp_install.reg ECHO Windows Registry Editor Version 5.00
+>> tmp_install.reg ECHO.
+>> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
+>> tmp_install.reg ECHO "DisplayVersion"="%softversion%"
+>> tmp_install.reg ECHO "Comments"="%softname%  (%DATE:~-4%/%DATE:~-7,-5%/%DATE:~-10,-8%)"
+>> tmp_install.reg ECHO "DisplayName"="%regkey% %softversion%"
+>> tmp_install.reg ECHO "UninstallString"="%SystemDrive%\\Program Files\\%regkey%\\uninstall.bat"
+>> tmp_install.reg ECHO "InstallLocation"="%SystemDrive%\\Program Files\\%regkey%"
+>> tmp_install.reg ECHO "InstallFolder"="%SystemDrive%\\Program Files\\%regkey%"
+>> tmp_install.reg ECHO "DisplayIcon"="%SystemDrive%\\Program Files\\%regkey%\\bin\\xournalpp.exe,0"
+>> tmp_install.reg ECHO "URLInfoAbout"="https://github.com/xournalpp/xournalpp"
+>> tmp_install.reg ECHO "Publisher"="Xournal Team"
+>> tmp_install.reg ECHO.
+regedit.exe /S "tmp_install.reg"
 
 
 ECHO END %date%-%time%
---
+++ xournalpp/uninstall.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+SET softname=Telegram
+SET regkey=Xournal++
+SET shortcut=%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\Xournal++.lnk
+
+
+REM Clean reg uninstall key
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%"
+IF %ERRORLEVEL% EQU 0 (
+  REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%" /F
+)
+
+REM Remove Shortcut
+IF EXIST "%shortcut%" DEL /F /Q "%shortcut%"
+
+REM Uninstall
+IF EXIST "%ProgramFiles%\%regkey%\Uninstall.exe" Uninstall.exe /S
+
+REM Auto Remove (last line)
+IF EXIST "%ProgramFiles%\%regkey%" RMDIR /S /Q "%ProgramFiles%\%regkey%"
