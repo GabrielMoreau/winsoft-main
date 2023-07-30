ab94889d4c55b9770118487e5fd3095f1e549ed4
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-07-30 15:13:44 +0200

    Remove desktop icon and better Variable

--- blender/install.bat
+++ blender/install.bat
@@ -18,12 +18,16 @@ EXIT /B
 
 ECHO BEGIN %date%-%time%
 
-SET softversion=91.5.1
-SET softpatch=1
+SET softversion=__VERSION__
+SET softpatch=__PATCH__
 
 
 ECHO Silent install %softname%
 msiexec /i "blender-%softversion%-windows-x64.msi" ALLUSERS=1 /qn /L*v "%logdir%\%softname%-MSI.log"
+
+ECHO Remove desktop shortcut
+IF EXIST "%PUBLIC%\Desktop\Blender*.lnk"          DEL /F /Q "%PUBLIC%\Desktop\Blender*.lnk"
+IF EXIST "%ALLUSERSPROFILE%\Desktop\Blender*.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\Blender*.lnk"
 
 
 ECHO END %date%-%time%
