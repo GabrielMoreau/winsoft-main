4b1f36dd9157d7768f65c70b9289b65d2227259e
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-08-04 16:40:06 +0200

    Update BalenaEtcher and change to portable version (more easy to install for all users)

--- balenaetcher/install.bat
+++ balenaetcher/install.bat
@@ -1,4 +1,3 @@
-REM @ECHO OFF
 
 REM
 REM   BalenaEtcher
@@ -18,12 +17,52 @@ EXIT /B
 
 ECHO BEGIN %date%-%time%
 
-SET softversion=91.5.1
-SET softpatch=1
+SET softversion=__VERSION__
+SET softpatch=__PATCH__
+SET regkey=balena-etcher
+SET shortcut=%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\%softname%.lnk
+SET process=balenaEtcher.exe
+
+
+ECHO Kill running process
+taskkill /T /F /IM %process%
+
+ECHO Clean old version before install
+CALL .\uninstall.bat
 
 
 ECHO Silent install %softname%
-balenaEtcher-Setup-%softversion%.exe /S
+MOVE "BalenaEtcher" "%ProgramFiles%\%regkey%"
+
+ECHO Copy uninstall script
+COPY /A /Y "uninstall.bat" "%ProgramFiles%\%regkey%\uninstall.bat"
+
+
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Create shortcut
+IF EXIST "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs" (
+  %pwrsh% -Command "$WS = New-Object -ComObject WScript.Shell; $SC = $WS.CreateShortcut('%shortcut%'); $SC.TargetPath = '%ProgramFiles%\%regkey%\balenaEtcher.exe'; $SC.Save();" -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
+)
+
+
+ECHO Better reg uninstall key
+ > tmp_install.reg ECHO Windows Registry Editor Version 5.00
+>> tmp_install.reg ECHO.
+>> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
+>> tmp_install.reg ECHO "DisplayVersion"="%softversion%"
+>> tmp_install.reg ECHO "Comments"="%softname%  (%DATE:~-4%/%DATE:~-7,-5%/%DATE:~-10,-8%)"
+>> tmp_install.reg ECHO "DisplayName"="balenaEtcher %softversion%"
+>> tmp_install.reg ECHO "UninstallString"="%SystemDrive%\\Program Files\\balena-etcher\\uninstall.bat"
+>> tmp_install.reg ECHO "InstallLocation"="%SystemDrive%\\Program Files\\balena-etcher"
+>> tmp_install.reg ECHO "InstallFolder"="%SystemDrive%\\Program Files\\balena-etcher"
+>> tmp_install.reg ECHO "DisplayIcon"="%SystemDrive%\\Program Files\\balena-etcher\\resources\\icons\\3.ico"
+>> tmp_install.reg ECHO "URLInfoAbout"="https://etcher.balena.io/"
+>> tmp_install.reg ECHO "Publisher"="Balena Ltd."
+>> tmp_install.reg ECHO.
+regedit.exe /S "tmp_install.reg"
 
 
 ECHO END %date%-%time%
---
+++ balenaetcher/view.ps1
@@ -0,0 +1,26 @@
+
+# View
+
+$RefName = 'BalenaEtcher'
+
+@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+  Get-ChildItem -Recurse "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall") |
+	ForEach {
+		$Key = $_
+		$App = (Get-ItemProperty -Path $Key.PSPath)
+		$DisplayName  = $App.DisplayName
+		If (!($DisplayName -match $RefName)) { Return }
+
+		$DisplayVersion = $App.DisplayVersion
+		$Publisher = $App.Publisher
+		$KeyProduct = $Key | Split-Path -Leaf
+		$Exe = $App.UninstallString
+		"# {0,-20} / {1,-14} / {2} / {3} (C) {4}" -F $DisplayName, $DisplayVersion, $KeyProduct, $Exe, $Publisher
+	} | Sort-Object
+
+Read-Host “Press ENTER to exit...”
+
+Return
+
+# balenaEtcher 1.18.8  / 1.18.8         / d2f3b6c7-6f49-59e2-b8a5-f72e33900c2b / "C:\Users\xxxxxxxxx\AppData\Local\Programs\balena-etcher\Uninstall balenaEtcher.exe" /currentuser (C) Balena Ltd.
