2c52a256300a5eda090f6790c8cf97fbe62fc987
Gabriel Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
2023-08-21 15:51:39 +0200

    Update LibreOffice

--- libreoffice/install.bat
+++ libreoffice/install.bat
@@ -18,17 +18,17 @@ EXIT /B
 
 ECHO BEGIN %date%-%time%
 
-SET softversion=7.1.3
-SET softpatch=1
+SET softversion=__VERSION__
+SET softpatch=__PATCH__
 
 SET process=soffice.bin
 
 REM https://wiki.documentfoundation.org/Deployment_and_Migration
 
-REM Silent install
+ECHO Silent install %softname%
 msiexec /qn /i "LibreOffice_%softversion%_Win_x86-64.msi" /norestart /l "%logdir%\%softname%-MSI.log" CREATEDESKTOPLINK=0 RebootYesNo=No ISCHECKFORPRODUCTUPDATES=0 REGISTER_NO_MSO_TYPES=1 REMOVE=gm_o_Onlineupdate SELECT_WORD=0 SELECT_EXCEL=0 SELECT_POWERPOINT=0 USERNAME="LEGI" ADDLOCAL=ALL
 
-REM Silent help install
+ECHO Silent help install
 msiexec /qn /i "LibreOffice_%softversion%_Win_x86-64_helppack_fr.msi" /norestart /l "%logdir%\%softname%-HELP.log"
 
 
