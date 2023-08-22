d27f6a272640a0cabaadc9193ef4dc29396a9b33
Gabriel Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
2023-08-22 10:50:32 +0200

    Add package for BleachBit

---
+++ bleachbit/README.md
@@ -0,0 +1,10 @@
+# BleachBit - Vector graphics editor
+
+BleachBit is a free and open-source disk space cleaner, privacy manager,
+and computer system optimizer. 
+
+* Website : https://www.bleachbit.org/
+* Wikipedia : https://en.wikipedia.org/wiki/BleachBit
+
+* Download : https://www.bleachbit.org/download/windows
+* Silent install : https://silentinstallhq.com/bleachbit-silent-install-how-to-guide/
---
+++ bleachbit/install.bat
@@ -0,0 +1,40 @@
+REM @ECHO OFF
+
+REM
+REM   BleachBit
+REM
+
+REM Name
+SET softname=BleachBit
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+SET softversion=__VERSION__
+SET softpatch=__PATCH__
+
+
+REM ECHO Silent uninstall previous version
+REM IF EXIST "%ProgramFiles%\BleachBit\uninstall.exe"      "%ProgramFiles%\BleachBit\uninstall.exe" /allusers /S
+REM IF EXIST "%ProgramFiles(x86)%\BleachBit\uninstall.exe" "%ProgramFiles(x86)%\BleachBit\uninstall.exe" /allusers /S
+
+
+ECHO Silent install %softname%
+BleachBit-%softversion%-setup.exe /allusers /S
+
+
+ECHO Remove desktop shortcut
+IF EXIST "%PUBLIC%\Desktop\BleachBit.lnk"          DEL /F /Q "%PUBLIC%\Desktop\BleachBit.lnk"
+IF EXIST "%ALLUSERSPROFILE%\Desktop\BleachBit.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\BleachBit.lnk"
+
+
+ECHO END %date%-%time%
+EXIT
