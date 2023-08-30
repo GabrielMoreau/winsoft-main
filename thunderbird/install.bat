19d606a0bead6291892df59e25a806fe4dae4956
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-08-30 11:15:20 +0200

    Update Chrome, Thunderbird and Arduino IDE

--- thunderbird/install.bat
+++ thunderbird/install.bat
@@ -18,8 +18,8 @@ EXIT /B
 
 ECHO BEGIN %date%-%time%
 
-SET softversion=91.5.1
-SET softpatch=1
+SET softversion=__VERSION__
+SET softpatch=__PATCH__
 SET process=thunderbird.exe
 
 
--- thunderbird/post-install.ps1
+++ thunderbird/post-install.ps1
@@ -1,7 +1,7 @@
 
 # Clean old duplicate key with Thunderbird in the name (same uninstall string)
 
-$RefVersion = '91.5.1'
+$RefVersion = '__VERSION__'
 $RefUninstallString = ''
 $RefName = 'Thunderbird'
 
