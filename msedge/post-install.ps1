be8878d6dc8e6c601830e2b15b7b79e59f9e09a7
De Marchi <olivier.de-marchi@grenoble-inp.fr>
2023-08-31 15:38:39 +0200

    add var RefVersion

--- msedge/post-install.ps1
+++ msedge/post-install.ps1
@@ -2,6 +2,8 @@
 # Post-install for add extension
 # https://learn.microsoft.com/en-us/deployedge/microsoft-edge-policies#extensioninstallforcelist
 
+$RefVersion = '116.0.1938.62'
+
 # Force install Ublock Origin on Edge
 # https://microsoftedge.microsoft.com/addons/detail/ublock-origin/odfafepnkmbhccpbejgmiehpchacaeak
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist")) {
