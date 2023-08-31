2c03b5991bb7c40805b1c322ceb0ff338350c968
De Marchi <olivier.de-marchi@univ-grenoble-alpes.fr>
2023-08-31 16:58:28 +0200

    string for REG_SZ

--- msedge/post-install.ps1
+++ msedge/post-install.ps1
@@ -9,4 +9,4 @@ $RefVersion = '116.0.1938.62'
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist")) {
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Force | Out-Null
 }
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name "1" -Type REG_SZ -Value "odfafepnkmbhccpbejgmiehpchacaeak"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name "1" -Type String -Value "odfafepnkmbhccpbejgmiehpchacaeak"
