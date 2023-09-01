1b613f7910d86da7a7c757b10322fb93a5927548
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-09-01 10:16:37 +0200

    Change comment order

--- msedge/post-install.ps1
+++ msedge/post-install.ps1
@@ -4,9 +4,11 @@
 
 $RefVersion = '__VERSION__'
 
-# Force install Ublock Origin on Edge
-# https://microsoftedge.microsoft.com/addons/detail/ublock-origin/odfafepnkmbhccpbejgmiehpchacaeak
 If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist")) {
+	# Create the key folder
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Force | Out-Null
 }
+
+# Force install Ublock Origin on MSEdge
+# https://microsoftedge.microsoft.com/addons/detail/ublock-origin/odfafepnkmbhccpbejgmiehpchacaeak
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name "1" -Type String -Value '"odfafepnkmbhccpbejgmiehpchacaeak"'
