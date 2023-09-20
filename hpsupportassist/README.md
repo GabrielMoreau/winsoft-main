36de917d18f8c0be25f07a2eab6e442e7d346294
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-09-20 13:56:07 +0200

    Add HP Support Assist - no auto detect new version

---
+++ hpsupportassist/README.md
@@ -0,0 +1,12 @@
+# HP Support Assistant - Assist for computer maintenance, software upgrades, troubleshooting problems
+
+The HP Support Assistant is recommended for all HP computers to assist
+with computer maintenance, software upgrades, troubleshooting problems,
+and many other options.
+
+* Website : https://support.hp.com/us-en/help/hp-support-assistant
+
+* Download : https://support.hp.com/us-en/help/hp-support-assistant
+	https://www.filehorse.com/download-hp-support-assistant/download/
+* Silent install : https://silentinstallhq.com/hp-support-assistant-silent-install-how-to-guide/
+	https://h30434.www3.hp.com/t5/Notebook-Software-and-How-To-Questions/How-to-silently-install-and-uni-nstall-HP-Support-Assistant/td-p/8022433
---
+++ hpsupportassist/install.bat
@@ -0,0 +1,40 @@
+REM @ECHO OFF
+
+REM
+REM   HPSupportAssist
+REM
+
+REM Name
+SET softname=HPSupportAssist
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+SET softversion=__VERSION__
+SET softpatch=__PATCH__
+
+
+ECHO Disabled automatic install, notifytray and notifypopup
+Reg.exe ADD "HKLM\SOFTWARE\WOW6432Node\Hewlett-Packard\HPActiveSupport\HPSF" /v NotifyTray /t REG_SZ /d 0 /f
+Reg.exe ADD "HKLM\SOFTWARE\WOW6432Node\Hewlett-Packard\HPActiveSupport\HPSF" /v NotifyPopup /t REG_SZ /d 0 /f
+Reg.exe ADD "HKLM\SOFTWARE\WOW6432Node\Hewlett-Packard\HPActiveSupport\HPHC" /v Install /t REG_SZ /d 0 /f
+
+ECHO Silent install %softname%
+REM Setup.exe /s /v /q
+InstallHPSA.exe /S /v/qn
+
+ECHO Remove desktop shortcut
+IF EXIST "%PUBLIC%\Desktop\HP*Support*Assistant.lnk"          DEL /F /Q "%PUBLIC%\Desktop\HP*Support*Assistant.lnk"
+IF EXIST "%ALLUSERSPROFILE%\Desktop\HP*Support*Assistant.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\HP*Support*Assistant.lnk"
+
+
+ECHO END %date%-%time%
+EXIT
