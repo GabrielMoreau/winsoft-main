38543a0936941670fcb3e197cd43e0912d0f51b9
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-10-05 15:11:16 +0200

    Add Proton Mail Bridge

---
+++ protonmailbridge/README.md
@@ -0,0 +1,10 @@
+# Proton Mail Bridge - Secure messaging client
+
+Proton Mail Bridge is a free and open-source desktop application that runs in the background, encrypting and decrypting messages as they enter and leave your computer.
+The server is a proprieary software hosted in Swistzerland.
+
+* Website : https://proton.me/
+* Wikipedia : https://en.wikipedia.org/wiki/Proton_Mail
+
+* Download : https://proton.me/mail/download
+* Forge : https://github.com/ProtonMail/WebClients
---
+++ protonmailbridge/install.bat
@@ -0,0 +1,35 @@
+
+REM
+REM   ProtonMailBridge
+REM
+
+REM Name
+SET softname=ProtonMailBridge
+
+SET logdir=%ProgramData%\OCS Inventory NG\Agent\DeployLog
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+
+SET softversion=__VERSION__
+SET softpatch=__PATCH__
+
+
+ECHO Silent install %softname%
+Bridge-Installer-%softversion%.exe /qn /norestart /L*v "%logdir%\%softname%-MSI.log"
+
+
+ECHO Remove desktop shortcut
+IF EXIST "%PUBLIC%\Desktop\Proton Mail Bridge.lnk"          DEL /F /Q "%PUBLIC%\Desktop\Proton Mail Bridge.lnk"
+IF EXIST "%ALLUSERSPROFILE%\Desktop\Proton Mail Bridge.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\Proton Mail Bridge.lnk"
+
+
+ECHO END %date%-%time%
+EXIT
