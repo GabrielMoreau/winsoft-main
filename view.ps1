b2c3a4f72650b7815067588ed32f624c9c912d57
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-10-06 16:32:58 +0200

    Update with graphical version from SWMB

--- view.ps1
+++ view.ps1
@@ -1,29 +1,81 @@
+# Copy from SWMB project : https://gitlab.in2p3.fr/resinfo-gt/swmb/resinfo-swmb/-/blob/master/Tasks/View-All-Software.ps1
 
-Echo "###### View local machine software"
-@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
-  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
-	ForEach {
-		$Key = $_
-		$App = (Get-ItemProperty -Path $Key.PSPath)
-		$DisplayName  = $App.DisplayName
+Function SWMB_ListSoftware {
+	# Set HKU drive if not exists
+	New-PSDrive -PSProvider 'Registry' -Name 'HKU' -Root 'HKEY_USERS' -ErrorAction 'SilentlyContinue' | Out-Null
 
-		$DisplayVersion = $App.DisplayVersion
-		$Publisher = $App.Publisher
-		$KeyProduct = $Key | Split-Path -Leaf
-		$Exe = $App.UninstallString
-		"# {0,-66} / {1,-16} / {2,-42} / (C) {3,-25} / {4}" -F $DisplayName, $DisplayVersion, $KeyProduct, $Publisher, $Exe
-	} | Sort-Object
+	# Init
+	$Soft = @()
 
-Echo "###### View current user software"
-@(Get-ChildItem -Recurse "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall") |
-	ForEach {
-		$Key = $_
-		$App = (Get-ItemProperty -Path $Key.PSPath)
-		$DisplayName  = $App.DisplayName
+	@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+	  Get-ChildItem -Recurse 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall') |
+		ForEach {
+			$Key = $_
+			$App = (Get-ItemProperty -Path $Key.PSPath)
+			$DisplayName    = $App.DisplayName
+			$DisplayVersion = $App.DisplayVersion
+			If ([string]::IsNullOrEmpty($DisplayName) -And [string]::IsNullOrEmpty($DisplayVersion)) { Return }
 
-		$DisplayVersion = $App.DisplayVersion
-		$Publisher = $App.Publisher
-		$KeyProduct = $Key | Split-Path -Leaf
-		$Exe = $App.UninstallString
-		"# {0,-66} / {1,-16} / {2,-42} / (C) {3,-25} / {4}" -F $DisplayName, $DisplayVersion, $KeyProduct, $Publisher, $Exe
-	} | Sort-Object
+			$Publisher  = $App.Publisher
+			$KeyProduct = $Key | Split-Path -Leaf
+			$Exe = $App.UninstallString
+			$Soft += New-Object PSObject -Property @{
+				DisplayName    = $DisplayName
+				DisplayVersion = $DisplayVersion
+				KeyProduct     = $KeyProduct
+				Publisher      = $Publisher
+				UninstallExe   = $Exe
+				Hive           = 'HKLM'
+				#KeyPath        = $Key.Name
+			}
+		}
+
+	@(Get-ChildItem -Recurse 'HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Uninstall') |
+		ForEach {
+			$Key = $_
+			$App = (Get-ItemProperty -Path $Key.PSPath)
+			$DisplayName    = $App.DisplayName
+			$DisplayVersion = $App.DisplayVersion
+			If ([string]::IsNullOrEmpty($DisplayName) -And [string]::IsNullOrEmpty($DisplayVersion)) { Return }
+
+			$Publisher  = $App.Publisher
+			$KeyProduct = $Key | Split-Path -Leaf
+			$Exe = $App.UninstallString
+			$Soft += New-Object PSObject -Property @{
+				DisplayName    = $DisplayName
+				DisplayVersion = $DisplayVersion
+				KeyProduct     = $KeyProduct
+				Publisher      = $Publisher
+				UninstallExe   = $Exe
+				Hive           = 'HKU'
+				#KeyPath        = $Key.Name
+			}
+		}
+
+	@(Get-ChildItem -Recurse 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall') |
+		ForEach {
+			$Key = $_
+			$App = (Get-ItemProperty -Path $Key.PSPath)
+			$DisplayName    = $App.DisplayName
+			$DisplayVersion = $App.DisplayVersion
+			If ([string]::IsNullOrEmpty($DisplayName) -And [string]::IsNullOrEmpty($DisplayVersion)) { Return }
+
+			$Publisher  = $App.Publisher
+			$KeyProduct = $Key | Split-Path -Leaf
+			$Exe = $App.UninstallString
+			$Soft += New-Object PSObject -Property @{
+				DisplayName    = $DisplayName
+				DisplayVersion = $DisplayVersion
+				KeyProduct     = $KeyProduct
+				Publisher      = $Publisher
+				UninstallExe   = $Exe
+				Hive           = 'HKCU'
+				#KeyPath        = $Key.Name
+			}
+		}
+
+	Return ($Soft | Select Hive,DisplayName,Publisher,DisplayVersion,KeyProduct,UninstallExe | Sort-Object -Property Hive,DisplayName)
+}
+
+# GUI Output
+SWMB_ListSoftware | Out-GridView -Title 'LocalMachine and CurrentUser Software'
