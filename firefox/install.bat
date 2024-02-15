e02fc245bd0d06e499f318c209f9d923aaf361d4
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-02-15 16:42:58 +0100

    Remove the SHORTCUT parameter

--- firefox/install.bat
+++ firefox/install.bat
@@ -45,7 +45,7 @@ ECHO Execute pre-install script (clean register policies)
 
 
 ECHO Silent install %softname%
-msiexec /i "Firefox-Setup-%softversion%-esr.msi" INSTALL_MAINTENANCE_SERVICE=false DESKTOP_SHORTCUT=true /q /norestart /L*v "%logdir%\%softname%-MSI.log"
+msiexec /i "Firefox-Setup-%softversion%-esr.msi" INSTALL_MAINTENANCE_SERVICE=false /q /norestart /L*v "%logdir%\%softname%-MSI.log"
 
 
 REM voir https://github.com/mozilla/policy-templates/blob/master/README.md
