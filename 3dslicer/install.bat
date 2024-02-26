e735fac4e8b2964fb86c8ad0459af4576c588aeb
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-02-26 09:38:12 +0100

    Remove old version of 3D Slicer

--- 3dslicer/install.bat
+++ 3dslicer/install.bat
@@ -1,4 +1,3 @@
-REM @ECHO OFF
 
 REM
 REM   3DSlicer
@@ -19,6 +18,21 @@ EXIT /B
 ECHO BEGIN %date%-%time%
 
 SET softversion=__VERSION__
+
+
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO unblock
+%pwrsh% "Unblock-File -Path .\*.ps1"
+
+
+ECHO Execute pre-install script
+%pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
 
 
 ECHO Silent install %softname%
---
+++ 3dslicer/pre-install.ps1
@@ -0,0 +1,73 @@
+
+Write-Output "Begin Pre-Install"
+
+$RefName = '^Slicer\s[1-9]'
+$RefVersion = '__VERSION__'
+
+Function ToVersion {
+	Param (
+		[Parameter(Mandatory = $true)] [string]$Version
+	)
+
+	$Version = $Version -Replace '[^\d\.].*$', ''
+	$Version = $Version -Replace '\.00?$', ''
+	$Version = $Version -Replace '\.00?$', ''
+	Return [version]$Version
+}
+
+# Remove old version
+@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+	ForEach {
+		$Key = $_
+		$App = (Get-ItemProperty -Path $Key.PSPath)
+		$DisplayName  = $App.DisplayName
+		If (!($DisplayName -match $RefName)) { Return }
+
+		$DisplayVersion = $App.DisplayVersion
+		$KeyProduct = $Key | Split-Path -Leaf
+
+		If ((ToVersion($DisplayVersion)) -ge (ToVersion($RefVersion))) { Return }
+
+		If ($($App.UninstallString) -match 'MsiExec.exe') {
+			$Exe = 'msiexec.exe'
+			$Args = '/x "' + $KeyProduct + '" /qn'
+			Write-Output "Remove MSI: $DisplayName / $DisplayVersion / $KeyProduct / $Exe $Args"
+		} Else {
+			$UninstallSplit = $App.UninstallString -Split "exe"
+			$Exe = $UninstallSplit[0] + 'exe"'
+			$Args = '/S'
+			Write-Output "Remove EXE: $DisplayName / $DisplayVersion / $($App.UninstallString) / $Exe $Args"
+		}
+
+		$Proc = Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue' -PassThru
+
+		$Timeouted = $Null # Reset any previously set timeout
+		# Wait up to 180 seconds for normal termination
+		$Proc | Wait-Process -Timeout 300 -ErrorAction SilentlyContinue -ErrorVariable Timeouted
+		If ($Timeouted) {
+			# Terminate the process
+			$Proc | Kill
+			Write-Output "Error: kill $RefName uninstall exe"
+			Return
+		} ElseIf ($Proc.ExitCode -ne 0) {
+			Write-Output "Error: $RefName uninstall return code $($Proc.ExitCode)"
+			Return
+		}
+	}
+
+# View
+@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+	ForEach {
+		$Key = $_
+		$App = (Get-ItemProperty -Path $Key.PSPath)
+		$DisplayName  = $App.DisplayName
+		If (!($DisplayName -match $RefName)) { Return }
+
+		$DisplayVersion = $App.DisplayVersion
+		$KeyProduct = $Key | Split-Path -Leaf
+		Write-Output "View: $DisplayName / $DisplayVersion / $KeyProduct / $($App.UninstallString)"
+	}
+
+# View: Slicer 5.6.0 / 5.6.0 / Slicer 5.6.0 (Win64) / "C:\ProgramData\Slicer.org\Uninstall.exe"
