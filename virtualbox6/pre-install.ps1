fac5d1205511d0381d81af3670b6dce6f7f7e416
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-02-26 16:03:59 +0100

    Update to the last version of Visual C++ Redistributable. Clean unused lines

--- virtualbox6/pre-install.ps1
+++ virtualbox6/pre-install.ps1
@@ -15,9 +15,9 @@ $ToDo = 'unknown'
 			#Write-Output "# $DisplayName / $DisplayVersion / $KeyProduct"
 
 			If ($DisplayName -match '2015-2019.*x64') {
-				If ($DisplayVersion -lt [version]'14.29.30153.0') {
+				If ($DisplayVersion -lt [version]'__2015-2022-x64__') {
 					If ($ToDo -eq 'unknown') {
-						$ToDo = 'install-2019'
+						$ToDo = 'install-2022'
 					}
 				} Else {
 					$ToDo = 'already-setup'
@@ -25,7 +25,7 @@ $ToDo = 'unknown'
 			}
 
 			ElseIf ($DisplayName -match '2015-2022.*x64') {
-				If ($DisplayVersion -lt [version]'14.38.33130.0') {
+				If ($DisplayVersion -lt [version]'__2015-2022-x64__') {
 					If ($ToDo -eq 'unknown') {
 						$ToDo = 'install-2022'
 					}
@@ -40,14 +40,14 @@ If ($ToDo -eq 'unknown') {
 	$ToDo = 'install-2022'
 }
 
-If ($ToDo -eq 'install-2019') {
-	$Exe = '2015-2019\vc_redist.x64.exe'
-	$Args = '/install /quiet /norestart'
-	If (Test-Path -Path "$Exe") {
-		Write-Output "Update Microsoft Visual C++ 2015-2019 (x64) redistributable"
-		Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue' -Wait
-	}
-}
+#If ($ToDo -eq 'install-2019') {
+#	$Exe = '2015-2019\vc_redist.x64.exe'
+#	$Args = '/install /quiet /norestart'
+#	If (Test-Path -Path "$Exe") {
+#		Write-Output "Update Microsoft Visual C++ 2015-2019 (x64) redistributable"
+#		Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue' -Wait
+#	}
+#}
 
 ElseIf ($ToDo -eq 'install-2022') {
 	$Exe = '2015-2022\vc_redist.x64.exe'
