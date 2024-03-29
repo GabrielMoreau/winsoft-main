f22c8d3590e279c6393e02bac75e8d662b087c16
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-03-29 15:52:48 +0100

    Add very simple shutdown script

---
+++ shutdown/README.md
@@ -0,0 +1,5 @@
+# Shutdown (Action) - Poweroff the computer
+
+Local deployment of this application allows you to switch off this computer.
+It can be used, for example, to remotely shut down computers
+that have forgotten to be turned off by their main user before a WE.
---
+++ shutdown/install.bat
@@ -0,0 +1,25 @@
+
+REM
+REM   Action-Shutdown
+REM
+
+REM Name
+SET softname=Action-Shutdown
+
+SET logdir=__LOGDIR__
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+
+shutdown /s /t 300 /c "Force Shutdown by __IT_TEAM__"
+
+
+ECHO END %date%-%time%
+EXIT
