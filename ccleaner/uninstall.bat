a67feafc9dce39fa42f8a9459d9b1bc7a7520ead
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-04-17 15:00:19 +0200

    Add uninstall script

---
+++ ccleaner/uninstall.bat
@@ -0,0 +1,26 @@
+
+REM
+REM   Uninstall-CCleaner
+REM
+
+REM Name
+SET softname=Uninstall-CCleaner
+
+SET logdir=__LOGDIR__
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+
+IF EXIST "%ProgramFiles%\CCleaner\uninst.exe"      ScriptRunner.exe -appvscript "%ProgramFiles%\CCleaner\uninst.exe"      /S -appvscriptrunnerparameters -wait -timeout=300
+IF EXIST "%ProgramFiles(x86)%\CCleaner\uninst.exe" ScriptRunner.exe -appvscript "%ProgramFiles(x86)%\CCleaner\uninst.exe" /S -appvscriptrunnerparameters -wait -timeout=300
+
+
+ECHO END %date%-%time%
+EXIT
