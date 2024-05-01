78c26de351d26f43d83929e037f0cb96f72bceda
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-05-01 16:15:28 +0200

    Add view at end install

--- firefox/post-install.ps1
+++ firefox/post-install.ps1
@@ -70,3 +70,17 @@ If ($RefUninstallString -ne '') {
 			}
 		}
 }
+
+# View
+@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+	ForEach {
+		$Key = $_
+		$App = (Get-ItemProperty -Path $Key.PSPath)
+		$DisplayName  = $App.DisplayName
+		If (!($DisplayName -match $RefName)) { Return }
+
+		$DisplayVersion = $App.DisplayVersion
+		$KeyProduct = $Key | Split-Path -Leaf
+		Write-Output "Installed: $DisplayName / $DisplayVersion / $KeyProduct / $($App.UninstallString)"
+	}
