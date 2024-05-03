0527283b82835b09af2eb2540e02dac2d24be0c8
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-05-03 09:13:16 +0200

    Update ps1 script to become static (signable)

--- dellcmdupdate/pre-install.ps1
+++ dellcmdupdate/pre-install.ps1
@@ -1,8 +1,45 @@
 
 Write-Output "Begin Pre-Install"
 
+# Get Config from file
+Function GetConfig {
+	Param (
+		[Parameter(Mandatory = $True)] [string]$FilePath
+	)
+
+	Return Get-Content "$FilePath" | Where-Object { $_ -Match '=' } | ForEach-Object { $_ -Replace "#.*", "" } | ForEach-Object { $_ -Replace "\\", "\\" } | ConvertFrom-StringData
+}
+
+# Run MSI or EXE with timeout control
+Function Run-Exec {
+	Param (
+		[Parameter(Mandatory = $True)] [string]$Name,
+		[Parameter(Mandatory = $True)] [string]$FilePath,
+		[Parameter(Mandatory = $True)] [string]$ArgumentList,
+		[Parameter(Mandatory = $False)] [int]$Timeout = 300
+	)
+
+	$Proc = Start-Process -FilePath "$FilePath" -ArgumentList "$ArgumentList" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue' -PassThru
+
+	$Timeouted = $Null # Reset any previously set timeout
+	# Wait up to 180 seconds for normal termination
+	$Proc | Wait-Process -Timeout $Timeout -ErrorAction SilentlyContinue -ErrorVariable Timeouted
+	If ($Timeouted) {
+		# Terminate the process
+		$Proc | Kill
+		Write-Output "Error: kill $Name uninstall exe"
+		Return
+	} ElseIf ($Proc.ExitCode -ne 0) {
+		Write-Output "Error: $Name uninstall return code $($Proc.ExitCode)"
+		Return
+	}
+}
+
+# Get Config: Version
+$Config = GetConfig -FilePath 'winsoft-config.ini'
+$RefVersion = $Config.Version
 $RefName = 'Dell Command . Update'
-$RefVersion = '__VERSION__'
+Write-Output "Config: Version $RefVersion"
 
 # Remove old version
 @(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
@@ -29,22 +66,8 @@ $RefVersion = '__VERSION__'
 			Write-Output "Remove: $DisplayName / $DisplayVersion / $($App.UninstallString) / $Exe $Args"
 		}
 
-		$Proc = Start-Process -FilePath "$Exe" -ArgumentList "$Args" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue' -PassThru
-
-		$Timeouted = $Null # Reset any previously set timeout
-		# Wait up to 180 seconds for normal termination
-		$Proc | Wait-Process -Timeout 300 -ErrorAction SilentlyContinue -ErrorVariable Timeouted
-		If ($Timeouted) {
-			# Terminate the process
-			$Proc | Kill
-			Write-Output "Error: kill $RefName uninstall exe"
-			Return
-		} ElseIf ($Proc.ExitCode -ne 0) {
-			Write-Output "Error: $RefName uninstall return code $($Proc.ExitCode)"
-			Return
-		}
+		Run-Exec -FilePath "$Exe" -ArgumentList "$Args" -Name "$RefName"
 	}
-
 
 # View
 @(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
--- digikam/post-install.ps1
+++ digikam/post-install.ps1
@@ -3,19 +3,46 @@ Write-Output "Begin Post-Install"
 
 # Clean old duplicate key with digiKam in the name (same uninstall string)
 
-$RefVersion = '__VERSION__'
+# Get Config from file
+Function GetConfig {
+	Param (
+		[Parameter(Mandatory = $True)] [string]$FilePath
+	)
+
+	Return Get-Content "$FilePath" | Where-Object { $_ -Match '=' } | ForEach-Object { $_ -Replace "#.*", "" } | ForEach-Object { $_ -Replace "\\", "\\" } | ConvertFrom-StringData
+}
+
+# Transform string to a version object
+Function ToVersion {
+	Param (
+		[Parameter(Mandatory = $true)] [string]$Version
+	)
+
+	$Version = $Version -Replace '[^\d\.].*$', ''
+	$Version = "$Version.0.0.0"
+	$Version = $Version -Replace '\.+',     '.'
+	$Version = $Version -Replace '\.0+',    '.0'
+	$Version = $Version -Replace '\.0(\d)', '.$1'
+	$Version = $Version.Split('.')[0,1,2,3] -Join '.'
+	Return [version]$Version
+}
+
+# Get Config: Version
+$Config = GetConfig -FilePath 'winsoft-config.ini'
+$RefVersion = $Config.Version
 $RefUninstallString = ''
+$RefName = 'digiKam'
+Write-Output "Config: Version $RefVersion"
 
 @(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
   Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
-	Where { $_.Name -match 'digiKam' } |
+	Where { $_.Name -match "$RefName" } |
 	ForEach {
 		$App = (Get-ItemProperty -Path $_.PSPath)
 		$DisplayVersion = $App.DisplayVersion
 		$Exe = $App.UninstallString
 		$DisplayName = $App.DisplayName
-		# Echo $DisplayName $DisplayVersion $Exe
-		If ([version]$DisplayVersion -eq [version]$RefVersion) {
+		If ((ToVersion($DisplayVersion)) -eq (ToVersion($RefVersion))) {
 			$RefUninstallString = $Exe
 			$KeyPath = $App.PSPath
 			Write-Output "Ref Key: $DisplayName / $Exe / $KeyPath"
@@ -25,17 +52,31 @@ $RefUninstallString = ''
 If ($RefUninstallString -ne '') {
 	@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
 	  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
-		Where { $_.Name -match 'digiKam' } |
+		Where { $_.Name -match "$RefName" } |
 		ForEach {
 			$App = (Get-ItemProperty -Path $_.PSPath)
 			$DisplayVersion = $App.DisplayVersion
 			$Exe = $App.UninstallString
 			$DisplayName = $App.DisplayName
 			# Echo "Check Key $DisplayName : $DisplayVersion < $RefVersion ?"
-			If (($Exe -eq $RefUninstallString) -And ([version]$DisplayVersion -lt [version]$RefVersion)) {
+			If (($Exe -eq $RefUninstallString) -And ((ToVersion($DisplayVersion)) -lt (ToVersion($RefVersion)))) {
 				$KeyPath = $App.PSPath
 				Write-Output "Remove Key: $DisplayName / $Exe / $KeyPath"
 				Remove-Item -Path "$KeyPath" -Force -Recurse -ErrorAction SilentlyContinue
 			}
 		}
 }
+
+# View
+@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+	ForEach {
+		$Key = $_
+		$App = (Get-ItemProperty -Path $Key.PSPath)
+		$DisplayName  = $App.DisplayName
+		If (!($DisplayName -match $RefName)) { Return }
+
+		$DisplayVersion = $App.DisplayVersion
+		$KeyProduct = $Key | Split-Path -Leaf
+		Write-Output "Installed: $DisplayName / $DisplayVersion / $KeyProduct / $($App.UninstallString)"
+	}
