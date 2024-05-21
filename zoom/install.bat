0a9a8bc801736305da40018d30598d1bf63426b5
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-05-21 17:23:16 +0200

    Remove Zoom Rooms

--- zoom/install.bat
+++ zoom/install.bat
@@ -22,7 +22,22 @@ REM https://support.zoom.us/hc/fr/articles/201362163-Installation-et-configurati
 SET softversion=__VERSION__
 
 
-REM Silent install
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO unblock
+%pwrsh% "Unblock-File -Path .\*.ps1"
+
+
+ECHO Execute pre-install script
+%pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+
+
+ECHO Silent install %softname%
 ScriptRunner.exe -appvscript MsiExec.exe /i ZoomInstallerFull-%softversion%.msi /quiet /qn /norestart /log "%logdir%\%softname%-MSI.log" MSIRESTARTMANAGERCONTROL="Disable" ZoomAutoUpdate="true" ZNoDesktopShortCut="true" ZSSOHOST="__ZSSOHOST__" ZConfig="nogoogle=1;nofacebook=1" ZRecommend="AudioAutoAdjust=1" -appvscriptrunnerparameters -wait -timeout=300
 
 
---
+++ zoom/pre-install.ps1
@@ -0,0 +1,63 @@
+
+Write-Output "Begin Pre-Install"
+
+# Run MSI or EXE with timeout control
+Function Run-Exec {
+	Param (
+		[Parameter(Mandatory = $True)] [string]$Name,
+		[Parameter(Mandatory = $True)] [string]$FilePath,
+		[Parameter(Mandatory = $True)] [string]$ArgumentList,
+		[Parameter(Mandatory = $False)] [int]$Timeout = 300
+	)
+
+	$Proc = Start-Process -FilePath "$FilePath" -ArgumentList "$ArgumentList" -WindowStyle 'Hidden' -ErrorAction 'SilentlyContinue' -PassThru
+
+	$Timeouted = $Null # Reset any previously set timeout
+	# Wait up to 180 seconds for normal termination
+	$Proc | Wait-Process -Timeout $Timeout -ErrorAction SilentlyContinue -ErrorVariable Timeouted
+	If ($Timeouted) {
+		# Terminate the process
+		$Proc | Kill
+		Write-Output "Error: kill $Name uninstall exe"
+		Return
+	} ElseIf ($Proc.ExitCode -ne 0) {
+		Write-Output "Error: $Name uninstall return code $($Proc.ExitCode)"
+		Return
+	}
+}
+
+$RefName = 'Zoom Rooms'
+# Remove only Zoom Rooms
+@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+	ForEach {
+		$Key = $_
+		$App = (Get-ItemProperty -Path $Key.PSPath)
+		$DisplayName  = $App.DisplayName
+		If (!($DisplayName -match $RefName)) { Return }
+
+		$DisplayVersion = $App.DisplayVersion
+		$KeyProduct = $Key | Split-Path -Leaf
+
+		If ($($App.UninstallString) -match 'MsiExec.exe') {
+			$Exe = 'MsiExec.exe'
+			$Args = '/x "' + $KeyProduct + '" /quiet Silent=true AcceptGDPR=true'
+			Write-Output "Remove MSI: $DisplayName / $DisplayVersion / $KeyProduct / $Exe $Args"
+			Run-Exec -FilePath "$Exe" -ArgumentList "$Args" -Name "$RefName"
+		}
+	}
+
+$RefName = 'Zoom'
+# View all Zoom
+@(Get-ChildItem -Recurse 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+  Get-ChildItem -Recurse "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+	ForEach {
+		$Key = $_
+		$App = (Get-ItemProperty -Path $Key.PSPath)
+		$DisplayName  = $App.DisplayName
+		If (!($DisplayName -match $RefName)) { Return }
+
+		$DisplayVersion = $App.DisplayVersion
+		$KeyProduct = $Key | Split-Path -Leaf
+		Write-Output "Installed: $DisplayName / $DisplayVersion / $KeyProduct / $($App.UninstallString)"
+	}
