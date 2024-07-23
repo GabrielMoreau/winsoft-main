f9ad719d5b37383d2317193119ac4fc3ba3c064e
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-07-23 12:12:01 +0200

    Add uninstall zoom package

---
+++ zoom/uninstall.bat
@@ -0,0 +1,37 @@
+
+REM
+REM   Uninstall-Zoom
+REM
+
+REM Name
+SET softname=Uninstall-Zoom
+
+SET logdir=__LOGDIR__
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO unblock
+%pwrsh% "Unblock-File -Path .\*.ps1"
+
+
+ECHO Execute pre-remove script
+%pwrsh% -File ".\pre-remove.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+
+
+ECHO END %date%-%time%
+EXIT
