089f132e0d649c0e1accf56bc8a61cc1cc06b75b
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-11-25 14:38:34 +0100

    Remove old version before installing new one

--- lycheeslicer/install.bat
+++ lycheeslicer/install.bat
@@ -20,8 +20,23 @@ ECHO BEGIN %date%-%time%
 SET softversion=__VERSION__
 
 
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO unblock
+%pwrsh% "Unblock-File -Path .\*.ps1"
+
+
+ECHO Execute pre-install script
+%pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+
+
 ECHO Silent install %softname%
-ScriptRunner.exe -appvscript LycheeSlicer-%softversion%.exe /S -appvscriptrunnerparameters -wait -timeout=300
+ScriptRunner.exe -appvscript LycheeSlicer-%softversion%.exe /allusers /S -appvscriptrunnerparameters -wait -timeout=300
 
 
 ECHO Remove desktop shortcut
