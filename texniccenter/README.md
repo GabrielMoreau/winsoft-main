04b1fa8725d3d77044135a732866e9345f7036b3
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-12-04 18:38:27 +0100

    Add TexNicCenter package

---
+++ texniccenter/README.md
@@ -0,0 +1,18 @@
+# TeXnicCenter - IDE for the LaTeX typesetting language
+
+TeXnicCenter is a free and open-source IDE for the LaTeX typesetting language.
+It uses the MiKTeX or TeX Live distributions.
+It allows the user to type documents in LaTeX and to compile them in PDF, DVI or PS. 
+
+* Website : https://www.texniccenter.org/
+* Wikipedia : https://en.wikipedia.org/wiki/TeXnicCenter
+
+* Download : https://www.texniccenter.org/download/
+
+
+## Register Key
+
+Example :
+
+ | Hive | DisplayName | Publisher | DisplayVersion | KeyProduct | UninstallExe |
+ |:---- |:----------- |:--------- |:-------------- |:---------- |:------------ |
---
+++ texniccenter/install.bat
@@ -0,0 +1,48 @@
+
+REM
+REM   TeXnicCenter
+REM
+
+REM Name
+SET softname=TeXnicCenter
+
+SET logdir=__LOGDIR__
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+SET softversion=__VERSION__
+
+
+ECHO Silent install %softname%
+"TXCSetup_%softversion%Stable_x64.exe" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
+
+REM IF EXIST "%PROGRAMFILES%\TeXnicCenter\Language" COPY /Y fr_FR.aff "%PROGRAMFILES%\TeXnicCenter\Language"
+REM IF EXIST "%PROGRAMFILES%\TeXnicCenter\Language" COPY /Y fr_FR.dic "%PROGRAMFILES%\TeXnicCenter\Language"
+REM IF EXIST "%PROGRAMFILES(x86)%\TeXnicCenter\Language" COPY /Y fr_FR.aff "%PROGRAMFILES(x86)%\TeXnicCenter\Language"
+REM IF EXIST "%PROGRAMFILES(x86)%\TeXnicCenter\Language" COPY /Y fr_FR.dic "%PROGRAMFILES(x86)%\TeXnicCenter\Language"
+
+
+
+
+REM Change Add and Remove values in the register
+
+REM > tmp_install.reg ECHO Windows Registry Editor Version 5.00
+REM >> tmp_install.reg ECHO.
+REM >> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
+REM >> tmp_install.reg ECHO "DisplayVersion"="%softversion% "
+REM >> tmp_install.reg ECHO "Comments"="Package OCS v%softpatch% (%DATE:~-4%/%DATE:~-7,-5%/%DATE:~-10,-8%)"
+REM >> tmp_install.reg ECHO "DisplayName"="%softname% (%softversion% OCS)"
+REM >> tmp_install.reg ECHO.
+
+REM regedit.exe /S "tmp_install.reg"
+
+
+ECHO END %date%-%time%
+EXIT
