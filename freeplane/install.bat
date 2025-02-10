632c888a573303c248e069736798b0e4cfa88e86
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2025-02-10 10:58:21 +0100

    New name for the binary setup

--- freeplane/install.bat
+++ freeplane/install.bat
@@ -21,7 +21,7 @@ SET softversion=__VERSION__
 
 
 ECHO Silent install %softname%
-ScriptRunner.exe -appvscript Freeplane-Setup-with-Java-%softversion%.exe  /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /MERGETASKS=!desktopicon /LOG="%logdir%\%softname%-MSI.log" -appvscriptrunnerparameters -wait -timeout=300
+ScriptRunner.exe -appvscript Freeplane-Setup-%softversion%.exe  /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /MERGETASKS=!desktopicon /LOG="%logdir%\%softname%-MSI.log" -appvscriptrunnerparameters -wait -timeout=300
 
 
 ECHO END %date%-%time%
