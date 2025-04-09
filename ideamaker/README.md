f7c2da790e900387627f444a78c05ac54103a4ed
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2025-04-09 09:57:40 +0200

    Remove old part with register key

--- ideamaker/README.md
+++ ideamaker/README.md
@@ -21,5 +21,7 @@ Example :
  |:---- |:----------- |:--------- |:-------------- |:---------- |:------------ |
  | HKLM | ideaMaker 5.0.6.8380 | Raise3D | 5.0.6.8380 | `ideaMaker` | `C:\Program Files\Raise3D\ideaMaker\uninstall.bat` |
  | HKLM | ideaMaker 5.0.6.8380 | Raise3D | 5.0.6.8380 | `ideaMaker-App` | `"C:\Program Files\Raise3D\ideaMaker-App\uninstall.exe"` |
- | HKLM | ideaMaker 5.1.2.8460 | Raise3D | 5.1.2.8460 | ideaMaker | `C:\Program Files\Raise3D\ideaMaker\uninstall.bat` |
- | HKLM | ideaMaker 5.1.2.8460 | Raise3D | 5.1.2.8460 | ideaMaker-App | `"C:\Program Files\Raise3D\ideaMaker-App\uninstall.exe"` |
+ | HKLM | ideaMaker 5.1.2.8460 | Raise3D | 5.1.2.8460 | `ideaMaker` | `C:\Program Files\Raise3D\ideaMaker\uninstall.bat` |
+ | HKLM | ideaMaker 5.1.2.8460 | Raise3D | 5.1.2.8460 | `ideaMaker-App` | `"C:\Program Files\Raise3D\ideaMaker-App\uninstall.exe"` |
+ | HKLM | ideaMaker 5.2.2.8570 | Raise3D | 5.2.2.8570 | `ideaMaker` | `C:\Program Files\Raise3D\ideaMaker\uninstall.bat` |
+ | HKLM | ideaMaker 5.2.2.8570 | Raise3D | 5.2.2.8570 | `ideaMaker-App` | `"C:\Program Files\Raise3D\ideaMaker-App\uninstall.exe"` |
--- ideamaker/install.bat
+++ ideamaker/install.bat
@@ -21,35 +21,24 @@ SET softversion=__VERSION__
 SET regkey=ideaMaker
 
 
+REM Clean reg old uninstall key we push in early version
+REM Now, software use register key ideaMaker-App
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ideaMaker"
+IF %ERRORLEVEL% EQU 0 (
+  REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ideaMaker" /F
+)
+
+REM Remove old uninstall.bat
+IF EXIST "%ProgramFiles%\Raise3D\ideaMaker\uninstall.bat" RMDIR /S /Q "%ProgramFiles%\Raise3D\ideaMaker"
+
+
 ECHO Silent install %softname%
 ScriptRunner.exe -appvscript install_ideaMaker_%softversion%.exe /S -appvscriptrunnerparameters -wait -timeout=300
 
 
-IF EXIST "%ProgramFiles%\Raise3D\ideaMaker\uninstall.exe" (
-  ECHO Copy uninstall script
-  COPY /A /Y "uninstall.bat" "%ProgramFiles%\Raise3D\ideaMaker\uninstall.bat"
-
-  ECHO Better reg uninstall key
-   > tmp_install.reg ECHO Windows Registry Editor Version 5.00
-  >> tmp_install.reg ECHO.
-  >> tmp_install.reg ECHO [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regkey%]
-  >> tmp_install.reg ECHO "DisplayVersion"="%softversion%"
-  >> tmp_install.reg ECHO "Comments"="%softname%  (%DATE:~-4%/%DATE:~-7,-5%/%DATE:~-10,-8%)"
-  >> tmp_install.reg ECHO "DisplayName"="ideaMaker %softversion%"
-  >> tmp_install.reg ECHO "UninstallString"="%SystemDrive%\\Program Files\\Raise3D\\ideaMaker\\uninstall.bat"
-  >> tmp_install.reg ECHO "InstallLocation"="%SystemDrive%\\Program Files\\Raise3D\\ideaMaker"
-  >> tmp_install.reg ECHO "InstallFolder"="%SystemDrive%\\Program Files\\Raise3D\\ideaMaker"
-  >> tmp_install.reg ECHO "DisplayIcon"="%SystemDrive%\\Program Files\\Raise3D\\ideaMaker\\ideaMaker.exe,0"
-  >> tmp_install.reg ECHO "URLInfoAbout"="https://www.raise3d.eu/"
-  >> tmp_install.reg ECHO "Publisher"="Raise3D"
-  >> tmp_install.reg ECHO.
-  regedit.exe /S "tmp_install.reg"
-)
-
 ECHO Remove desktop shortcut
-IF EXIST "%PUBLIC%\Desktop\ideaMaker.lnk"          DEL /F /Q "%PUBLIC%\Desktop\ideaMaker.lnk"
-IF EXIST "%ALLUSERSPROFILE%\Desktop\ideaMaker.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\ideaMaker.lnk"
-
+IF EXIST "%PUBLIC%\Desktop\ideaMaker*.lnk"          DEL /F /Q "%PUBLIC%\Desktop\ideaMaker*.lnk"
+IF EXIST "%ALLUSERSPROFILE%\Desktop\ideaMaker*.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\ideaMaker*.lnk"
 
 
 ECHO END %date%-%time%
