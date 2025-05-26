01fc97ed22a116b0e200e83a9aab0b52f3fcf1f0
Olivier De Marchi <olivier.de-marchi@univ-grenoble-alpes.fr>
2025-05-26 15:03:28 +0200

    Use module HardwareReadiness See Ref #2

--- action-windows11fullupgrade/pre-install.ps1
+++ action-windows11fullupgrade/pre-install.ps1
@@ -21,14 +21,35 @@ If (!(Get-Tpm).TpmReady) {
 	Exit 13
 }
 
-# Bypass TPM
-$CheckTPM_Version = (Get-Tpm).ManufacturerVersionFull20
-If (($CheckTPM_Version -like '*not supported*') -Or ($CheckTPM_Version -like '*non pris*')) {
-	Write-Output 'Warning: TPM not 2.0 - Registry bypass before install'
-	If (!(Test-Path 'HKLM:\SYSTEM\Setup\MoSetup')) {
-		New-Item -Path 'HKLM:\SYSTEM\Setup\MoSetup' -Force | Out-Null
-	}
-	Set-ItemProperty -Path 'HKLM:\SYSTEM\Setup\MoSetup' -Name 'AllowUpgradesWithUnsupportedTPMOrCPU' -Type DWord -Value  1
-} Else {
-	Write-Output "TPM >= 2.0 - Normal install Continue"
+Import-Module .\HardwareReadiness
+
+if ($(Get-HardwareReadiness).IsCapable -eq $true) {
+	Write-Output 'All requirements ready for upgrade to Windows 11 !'
 }
+Else {
+	Write-Output 'not ready'
+	$data = Get-HardwareReadiness
+	$Reason = $data.Reason
+	Write-Output "Reason : $Reason"
+	if (($Reason -like '*TPM not compatible*') -Or ($Reason -like '*Processor not compatible*')){
+		If (!(Test-Path 'HKLM:\SYSTEM\Setup\MoSetup')) {
+		New-Item -Path 'HKLM:\SYSTEM\Setup\MoSetup' -Force | Out-Null
+		}
+	Write-Output 'add AllowUpgradesWithUnsupportedTPMOrCPU in registry'
+	Set-ItemProperty -Path 'HKLM:\SYSTEM\Setup\MoSetup' -Name 'AllowUpgradesWithUnsupportedTPMOrCPU' -Type DWord -Value  1
+	}
+}
+
+
+
+# Bypass TPM
+#$CheckTPM_Version = (Get-Tpm).ManufacturerVersionFull20
+#If (($CheckTPM_Version -like '*not supported*') -Or ($CheckTPM_Version -like '*non pris*')) {
+#	Write-Output 'Warning: TPM not 2.0 - Registry bypass before install'
+#	If (!(Test-Path 'HKLM:\SYSTEM\Setup\MoSetup')) {
+#		New-Item -Path 'HKLM:\SYSTEM\Setup\MoSetup' -Force | Out-Null
+#	}
+#	Set-ItemProperty -Path 'HKLM:\SYSTEM\Setup\MoSetup' -Name 'AllowUpgradesWithUnsupportedTPMOrCPU' -Type DWord -Value  1
+#} Else {
+#	Write-Output "TPM >= 2.0 - Normal install Continue"
+#}
