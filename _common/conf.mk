3cd32c17accada65cade0231f87eb4da9a1e671a
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2025-08-16 12:10:57 +0200

    Add support for Uninstall Package

--- _common/conf.mk
+++ _common/conf.mk
@@ -44,6 +44,7 @@ sinclude $(SELF_MAKEDIR)../../winsoft-conf/_common/conf.mk
 ocs:
 	@echo ""
 	@echo "Name:     $(OCS_NAME)"
+	@[ ! -s "Uninstall-$(OCS_NAME).zip" ] || echo "Name:     Uninstall-$(OCS_NAME)"
 	@[ "$(OCS_PRIORITY)" -eq 5 ] || echo "Priority: *$(OCS_PRIORITY)*"
 	@echo "Launch:   install.bat"
 	@echo "Notify:   $(OCS_NOTIFY)"
