524fc26cd8cc1b909d15159233701e0129d6121f
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2025-08-26 23:11:18 +0200

    Add uninstall script

---
+++ tabby/uninstall.bat
@@ -0,0 +1,30 @@
+
+REM
+REM   Uninstall-Tabby
+REM
+
+REM Name
+SET softname=Uninstall-Tabby
+
+SET logdir=__LOGDIR__
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+ECHO BEGIN %date%-%time%
+
+SET softversion=__VERSION__
+
+
+ECHO Silent uninstall %softname%
+IF EXIST "%PROGRAMFILES%\Tabby\Uninstall Tabby.exe" (
+  ScriptRunner.exe -appvscript "%PROGRAMFILES%\Tabby\Uninstall Tabby.exe" /S /ALLUSERS -appvscriptrunnerparameters -wait -timeout=300
+)
+
+
+ECHO END %date%-%time%
+EXIT
