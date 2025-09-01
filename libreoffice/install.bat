9896cba8c12057204b38b17c66f50f9ccec85334
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2025-09-01 16:52:18 +0200

    Better install

--- libreoffice/install.bat
+++ libreoffice/install.bat
@@ -18,17 +18,36 @@ EXIT /B
 ECHO BEGIN %date%-%time%
 
 SET softversion=__VERSION__
-
 SET process=soffice.bin
+SET MAX_RETRY=1
 
 
+:INSTALL
 ECHO Silent install %softname%
 REM https://wiki.documentfoundation.org/Deployment_and_Migration
 ScriptRunner.exe -appvscript MsiExec.exe /i "LibreOffice_%softversion%_Win_x86-64.msi" /qn /norestart /l "%logdir%\%softname%-MSI.log" CREATEDESKTOPLINK=0 RebootYesNo=No ISCHECKFORPRODUCTUPDATES=0 REGISTER_NO_MSO_TYPES=1 REMOVE=gm_o_Onlineupdate SELECT_WORD=0 SELECT_EXCEL=0 SELECT_POWERPOINT=0 USERNAME="LEGI" ADDLOCAL=ALL -appvscriptrunnerparameters -wait -timeout=450
 
+ECHO Check if installed
+IF EXIST "%ProgramFiles%\LibreOffice\program\soffice.exe" (
+    GOTO HELP
+) ELSE (
+    IF "%MAX_RETRY%"=="0" (
+        ECHO Error - MAX_RETRY installation done and no %softname%
+        ECHO END %date%-%time%
+        EXIT /B 1
+    ) else (
+        ECHO Warning - try install again
+        SET /A MAX_RETRY-=1
+        goto INSTALL
+    )
+)
+
+
+:HELP
 ECHO Silent help install
 ScriptRunner.exe -appvscript MsiExec.exe /i "LibreOffice_%softversion%_Win_x86-64_helppack_fr.msi" /qn /norestart /l "%logdir%\%softname%-HELP.log" -appvscriptrunnerparameters -wait -timeout=300
 
 
+:END
 ECHO END %date%-%time%
 EXIT
