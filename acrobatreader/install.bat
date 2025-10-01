4c8271108e1e56aa84838cbf8c4fad9c2ed63c41
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2025-10-01 10:51:58 +0200

    Add taskkill and if not install, try again one time

--- acrobatreader/install.bat
+++ acrobatreader/install.bat
@@ -19,6 +19,11 @@ ECHO BEGIN %date%-%time%
 
 
 SET softversion=__VERSION__
+SET MAX_RETRY=1
+
+
+ECHO Kill running process
+TASKKILL /T /F /IM Acrobat.exe /IM AcroCEF.exe /IM AdobeCollabSync.exe
 
 
 ECHO Search PowerShell
@@ -36,10 +41,28 @@ ECHO Execute pre-install script
 %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
 
 
+:REINSTALL
 ECHO Silent Install %softname%
 ScriptRunner.exe -appvscript AcroRdrDCx64%softversion%_fr_FR.exe /sAll /rs /msi EULA_ACCEPT=YES DISABLEDESKTOPSHORTCUT=1 /L*V "%logdir%\%softname%-MSI.log" -appvscriptrunnerparameters -wait -timeout=600
 
 
+ECHO Check if installed
+IF EXIST "%ProgramFiles%\Adobe\Acrobat DC\Acrobat\Acrobat.exe"" (
+  GOTO POSTINSTALL
+) ELSE (
+  IF "%MAX_RETRY%"=="0" (
+    ECHO Error: MAX_RETRY installation done and no %softname%
+    ECHO END %date%-%time%
+    EXIT /B 1
+  ) ELSE (
+    ECHO Warning: try installation again
+    SET /A MAX_RETRY-=1
+    GOTO REINSTALL
+  )
+)
+
+
+:POSTINSTALL
 ECHO Execute post-install script
 %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
 
