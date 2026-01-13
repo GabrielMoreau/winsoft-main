e27e6c5c4b3bbb3e9a4fd7c5f013dc2c7c04299b
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-13 11:15:33 +0100

    Add returncode 141 142 and 143

--- libreoffice/install.bat
+++ libreoffice/install.bat
@@ -17,24 +17,40 @@ EXIT /B
 
 ECHO BEGIN %date%-%time%
 
+
 SET softversion=__VERSION__
 SET process=soffice.bin
 SET MAX_RETRY=1
 
 
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO unblock
+%pwrsh% "Unblock-File -Path .\*.ps1"
+
+
 :REINSTALL
-ECHO Silent install %softname%
+ECHO Silent Install %softname%
 REM https://wiki.documentfoundation.org/Deployment_and_Migration
-ScriptRunner.exe -appvscript MsiExec.exe /i "LibreOffice_%softversion%_Win_x86-64.msi" /qn /norestart /l "%logdir%\%softname%-MSI.log" CREATEDESKTOPLINK=0 RebootYesNo=No ISCHECKFORPRODUCTUPDATES=0 REGISTER_NO_MSO_TYPES=1 REMOVE=gm_o_Onlineupdate SELECT_WORD=0 SELECT_EXCEL=0 SELECT_POWERPOINT=0 USERNAME="LEGI" ADDLOCAL=ALL -appvscriptrunnerparameters -wait -timeout=450
+IF EXIST "LibreOffice_%softversion%_Win_x86-64.msi" (
+  ScriptRunner.exe -appvscript MsiExec.exe /i "LibreOffice_%softversion%_Win_x86-64.msi" /qn /norestart /l "%logdir%\%softname%-MSI.log" CREATEDESKTOPLINK=0 RebootYesNo=No ISCHECKFORPRODUCTUPDATES=0 REGISTER_NO_MSO_TYPES=1 REMOVE=gm_o_Onlineupdate SELECT_WORD=0 SELECT_EXCEL=0 SELECT_POWERPOINT=0 USERNAME="LEGI" ADDLOCAL=ALL -appvscriptrunnerparameters -wait -timeout=450
+) ELSE (
+    ECHO Error: installer is not in the archive!
+)
 
 ECHO Check if installed
 IF EXIST "%ProgramFiles%\LibreOffice\program\soffice.exe" (
-  GOTO HELP
+  GOTO POSTINSTALL
 ) ELSE (
   IF "%MAX_RETRY%"=="0" (
     ECHO Error: MAX_RETRY installation done and no %softname%
-    ECHO END %date%-%time%
-    EXIT /B 1
+    SET RETURNCODE=140
+    GOTO END
   ) ELSE (
     ECHO Warning: try installation again
     SET /A MAX_RETRY-=1
@@ -43,11 +59,18 @@ IF EXIST "%ProgramFiles%\LibreOffice\program\soffice.exe" (
 )
 
 
+:POSTINSTALL
+ECHO Execute post-install script
+%pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
 :HELP
 ECHO Silent help install
 ScriptRunner.exe -appvscript MsiExec.exe /i "LibreOffice_%softversion%_Win_x86-64_helppack_fr.msi" /qn /norestart /l "%logdir%\%softname%-HELP.log" -appvscriptrunnerparameters -wait -timeout=300
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 :END
 ECHO END %date%-%time%
-EXIT
+EXIT %RETURNCODE%
