cdfe0cc6c371b29722e06f57c02537a485be0caa
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-13 10:13:31 +0100

    Add return code if Zotero is not installed

--- zotero/install.bat
+++ zotero/install.bat
@@ -24,10 +24,17 @@ ECHO Silent install %softname%
 ScriptRunner.exe -appvscript Zotero-%softversion%_x64_setup.exe /S -appvscriptrunnerparameters -wait -timeout=300
 
 
+:POSTINSTALL
+ECHO Execute post-install script
+%pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+SET RETURNCODE=%ERRORLEVEL%
+
+
 ECHO Remove desktop shortcut
 IF EXIST "%PUBLIC%\Desktop\%softname%.lnk"          DEL /F /Q "%PUBLIC%\Desktop\%softname%.lnk"
 IF EXIST "%ALLUSERSPROFILE%\Desktop\%softname%.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\%softname%.lnk"
 
 
+:END
 ECHO END %date%-%time%
-EXIT
+EXIT %RETURNCODE%
