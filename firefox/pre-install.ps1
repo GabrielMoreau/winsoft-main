932298e921d79c834bc91dbdce49c1381e773afe
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-13 18:00:55 +0100

    Add View already Firefox (use when debug)

--- firefox/pre-install.ps1
+++ firefox/pre-install.ps1
@@ -1,5 +1,42 @@
 
-Write-Output "Begin Pre-Install"
+$TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+Write-Output ("Begin Pre-Install [$TimeStamp]`n" + "=" * 39 + "`n")
+
+########################################################################
+
+# Get Config from file
+Function GetConfig {
+	Param (
+		[Parameter(Mandatory = $True)] [string]$FilePath
+	)
+
+	Return Get-Content "$FilePath" | Where-Object { $_ -Match '=' } | ForEach-Object { $_ -Replace "#.*", "" } | ForEach-Object { $_ -Replace "\\", "\\" } | ConvertFrom-StringData
+}
+
+# Transform string to a version object
+Function ToVersion {
+	Param (
+		[Parameter(Mandatory = $true)] [string]$Version
+	)
+
+	$Version = $Version -Replace '[^\d\.].*$', ''
+	$Version = "$Version.0.0.0"
+	$Version = $Version -Replace '\.+',     '.'
+	$Version = $Version -Replace '\.0+',    '.0'
+	$Version = $Version -Replace '\.0(\d)', '.$1'
+	$Version = $Version.Split('.')[0,1,2,3] -Join '.'
+	Return [version]$Version
+}
+
+########################################################################
+
+$UninstallKeys = @(
+	'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+	'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+)
+
+########################################################################
+
 
 # Remove Policies by register key, only by policies.json
 # https://support.mozilla.org/en-US/questions/1302600
@@ -8,3 +45,24 @@ If (Test-Path 'HKLM:\SOFTWARE\Policies\Mozilla\Firefox\') {
 	Write-Output "Remove: Register Firefox Policies"
 	Remove-Item -Path 'HKLM:\SOFTWARE\Policies\Mozilla' -Force -Recurse -ErrorAction SilentlyContinue
 }
+
+# Get Config: Version
+$Config = GetConfig -FilePath 'winsoft-config.ini'
+$RefVersion = ToVersion $Config.Version
+$RefName = 'Firefox'
+Write-Output "Config: Version $RefVersion"
+
+
+# View
+$ReturnCode = 0
+ForEach ($Key in Get-ChildItem -Recurse $UninstallKeys) {
+	$App = Get-ItemProperty -Path $Key.PSPath
+	If ($App.DisplayName -notmatch $RefName) { Continue }
+
+	$DisplayVersion = ToVersion $App.DisplayVersion
+	$KeyProduct     = $Key.PSChildName
+	Write-Output "Installed: $($App.DisplayName) / $DisplayVersion / $KeyProduct / $($App.UninstallString)"
+}
+
+Write-Output "ReturnCode: $ReturnCode"
+Exit $ReturnCode
