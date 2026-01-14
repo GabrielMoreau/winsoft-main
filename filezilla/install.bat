b0d5188722ac6b8f3617f36887353562cefecdf8
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-14 18:14:35 +0100

    Activate returncode and so rewrite powershell script

--- filezilla/install.bat
+++ filezilla/install.bat
@@ -36,8 +36,20 @@ IF EXIST "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" (
 )
 
 
-ECHO Silent install
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+
+
+ECHO Silent install %softname%
 ScriptRunner.exe -appvscript "%softexe%" /user=all /S -appvscriptrunnerparameters -wait -timeout=300
+SET RETURNCODE=%ERRORLEVEL%
 
 
 ECHO Disable welcome and check update
@@ -48,20 +60,12 @@ IF EXIST "%ProgramFiles(x86)%\FileZilla FTP Client" (
   COPY /A /Y "fzdefaults.xml" "%ProgramFiles(x86)%\FileZilla FTP Client\fzdefaults.xml"
 )
 
-
-ECHO Search PowerShell
-SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
-IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
-
-ECHO Add rights
-%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
-
-ECHO unblock
-%pwrsh% "Unblock-File -Path .\*.ps1"
-
+:POSTINSTALL
 ECHO Execute post-install script
 %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
+:END
 ECHO END %date%-%time%
-EXIT
+EXIT %RETURNCODE%
