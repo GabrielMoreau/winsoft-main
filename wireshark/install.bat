2cd52aaecb631f0e5eb9866839a5951e953404a5
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-16 07:52:53 +0100

    Add deploy error code support

--- wireshark/install.bat
+++ wireshark/install.bat
@@ -23,17 +23,36 @@ REM NPCAP or Win10Pcap
 SET softversion2=__VERSION2__
 
 
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO unblock
+%pwrsh% "Unblock-File -Path .\*.ps1"
+
+
 ECHO Silent install NPcap or Win10Pcap
 REM npcap-%softversion2%.exe /S;
 IF NOT EXIST "%ProgramFiles%\Npcap\npcap.cat" (
   ECHO Silent install Win10Pcap
   ScriptRunner.exe -appvscript MsiExec.exe /i Win10Pcap-v%softversion2%.msi ALLUSERS=1 /qn /L*v "%logdir%\%softname%-MSI2.log" -appvscriptrunnerparameters -wait -timeout=300 2>&1
+  SET RETURNCODE=%ERRORLEVEL%
 )
 
 
 ECHO Silent install %softname%
 ScriptRunner.exe -appvscript Wireshark-%softversion%-x64.exe /S -appvscriptrunnerparameters -wait -timeout=300
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:POSTINSTALL
+ECHO Execute post-install script
+%pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 ECHO END %date%-%time%
-EXIT
+EXIT %RETURNCODE%
