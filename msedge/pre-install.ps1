9fe4b46f3c95b98d3d5c24573e73d160c52b128d
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-19 10:54:49 +0100

    Add View function

--- msedge/pre-install.ps1
+++ msedge/pre-install.ps1
@@ -1,5 +1,42 @@
 
-Write-Output "Begin Pre-Install"
+$TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+Write-Output ("Begin Pre-Install [$TimeStamp]`n" + "=" * 39 + "`n")
+
+########################################################################
+
+# Get Config from file
+Function GetConfig {
+	Param (
+		[Parameter(Mandatory = $True)] [string]$FilePath
+	)
+
+	Return Get-Content "$FilePath" | Where-Object { $_ -Match '=' } | ForEach-Object { $_ -Replace "#.*", "" } | ForEach-Object { $_ -Replace "\\", "\\" } | ConvertFrom-StringData
+}
+
+# Transform string to a version object
+Function ToVersion {
+	Param (
+		[Parameter(Mandatory = $true)] [string]$Version
+	)
+
+	$Version = $Version -Replace '[^\d\.].*$', ''
+	$Version = "$Version.0.0.0"
+	$Version = $Version -Replace '\.+',     '.'
+	$Version = $Version -Replace '\.0+',    '.0'
+	$Version = $Version -Replace '\.0(\d)', '.$1'
+	$Version = $Version.Split('.')[0,1,2,3] -Join '.'
+	Return [version]$Version
+}
+
+########################################################################
+
+$UninstallKeys = @(
+	'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+	'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+)
+
+########################################################################
+
 
 # Pre-install script for add extension to MSEdge
 # https://learn.microsoft.com/en-us/deployedge/microsoft-edge-policies#extensioninstallforcelist
@@ -35,3 +72,25 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallF
 # https://microsoftedge.microsoft.com/addons/detail/clearurls/mdkdmaickkfdekbjdoojfalpbkgaddei
 #Write-Output "Info: set addon ClearURLs"
 #Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name "5" -Value 'mdkdmaickkfdekbjdoojfalpbkgaddei;https://edge.microsoft.com/extensionwebstorebase/v1/crx'
+
+
+# Get Config: Version
+$Config = GetConfig -FilePath 'winsoft-config.ini'
+$RefVersion = ToVersion $Config.Version
+$RefName = 'Microsoft Edge'
+Write-Output "Config: Version $RefVersion"
+
+
+# View
+$ReturnCode = 0
+ForEach ($Key in Get-ChildItem -Recurse $UninstallKeys) {
+	$App = Get-ItemProperty -Path $Key.PSPath
+	If ($App.DisplayName -notmatch $RefName) { Continue }
+
+	$DisplayVersion = ToVersion $App.DisplayVersion
+	$KeyProduct     = $Key.PSChildName
+	Write-Output "Installed: $($App.DisplayName) / $DisplayVersion / $KeyProduct / $($App.UninstallString)"
+}
+
+Write-Output "ReturnCode: $ReturnCode"
+Exit $ReturnCode
