876fed0b29529bca9892db31a12f9dd877649df9
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-19 11:57:38 +0100

    Add support for deploy error code

--- advancedrenamer/install.bat
+++ advancedrenamer/install.bat
@@ -20,8 +20,31 @@ ECHO BEGIN %date%-%time%
 SET softversion=__VERSION__
 
 
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
+
+
 ECHO Silent install %softname%
 ScriptRunner.exe -appvscript advanced_renamer_setup_%softversion%.exe /SP- /VERYSILENT /SUPRESSMSGBOXES /NORESTART -appvscriptrunnerparameters -wait -timeout=300
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:POSTINSTALL
+ECHO Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 ECHO Remove desktop shortcut
@@ -29,5 +52,6 @@ IF EXIST "%PUBLIC%\Desktop\Advanced-Renamer.lnk"          DEL /F /Q "%PUBLIC%\De
 IF EXIST "%ALLUSERSPROFILE%\Desktop\Advanced-Renamer.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\Advanced-Renamer.lnk"
 
 
+:END
 ECHO END %date%-%time%
-EXIT
+EXIT %RETURNCODE%
