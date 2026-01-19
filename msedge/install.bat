4d744c23b58c201b7bbdf3d6622bbdf67e949d83
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-19 10:54:58 +0100

    Add support for deploy error code

--- msedge/install.bat
+++ msedge/install.bat
@@ -29,20 +29,34 @@ ECHO Add rights
 
 ECHO unblock
 %pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
 
 
 ECHO Execute pre-install script
-%pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 ECHO Silent install %softname%
 ScriptRunner.exe -appvscript MsiExec.exe /i MicrosoftEdgeEnterpriseX64-%softversion%.msi DONOTCREATEDESKTOPSHORTCUT=true DONOTCREATETASKBARSHORTCUT=true /qn /L*v "%logdir%\%softname%-MSI.log" -appvscriptrunnerparameters -wait -timeout=300
-
-
-ECHO END %date%-%time%
-
-IF %ERRORLEVEL% EQU 1603 (
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+IF %RETURNCODE% EQU 1603 (
   ECHO 0 or 1603 are good exit code for %softname% installer!
-  EXIT 0
+  SET RETURNCODE=0
 )
-EXIT
+
+
+:POSTINSTALL
+ECHO Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+
+:END
+ECHO END %date%-%time%
+EXIT %RETURNCODE%
