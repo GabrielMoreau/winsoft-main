06dc9fd3b1665198ce8139d6f6db7e3f1acd4ee1
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-25 16:30:50 +0100

    Update and move to deploy error return, .ini config, generic powershell script

--- gimp/install.bat
+++ gimp/install.bat
@@ -17,7 +17,6 @@ EXIT /B
 
 ECHO BEGIN %date%-%time%
 
-
 SET softversion=__VERSION__
 
 
@@ -34,11 +33,23 @@ SET RETURNCODE=0
 
 
 ECHO Execute pre-install script
-%pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 ECHO Silent install %softname%
 ScriptRunner.exe -appvscript gimp-%softversion%-setup.exe /VERYSILENT /NORESTART /ALLUSERS /MERGETASKS=!desktopicon /LOG="%logdir%\%softname%-MSI.log" -appvscriptrunnerparameters -wait -timeout=600
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:POSTINSTALL
+ECHO Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 ECHO Remove desktop shortcut
@@ -46,5 +57,6 @@ IF EXIST "%PUBLIC%\Desktop\GIMP *.lnk"          DEL /F /Q "%PUBLIC%\Desktop\GIMP
 IF EXIST "%ALLUSERSPROFILE%\Desktop\GIMP *.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\GIMP *.lnk"
 
 
+:END
 ECHO END %date%-%time%
-EXIT
+EXIT %RETURNCODE%
