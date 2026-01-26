a35db6ce6100a9ce06b116b251e9364cbe8a3796
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-26 17:01:48 +0100

    Add support for deploy error code and more generic files

--- notepadpp/install.bat
+++ notepadpp/install.bat
@@ -27,6 +27,23 @@ ECHO Kill running process
 TASKKILL /T /F /IM notepad++.exe
 
 
+ECHO Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+ECHO Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+ECHO Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
+
+
+ECHO Execute pre-install script
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
 ECHO Uninstall previous version
 IF EXIST "%ProgramFiles%\Notepad++\uninstall.exe" (
   ScriptRunner.exe -appvscript "%ProgramFiles%\Notepad++\uninstall.exe" /S -appvscriptrunnerparameters -wait -timeout=300
@@ -52,6 +69,17 @@ ping 127.0.0.1 -n 3 > NUL
 
 ECHO Silent install %softname%
 ScriptRunner.exe -appvscript "%softexe%" /S -appvscriptrunnerparameters -wait -timeout=300
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:POSTINSTALL
+ECHO Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 ECHO Disable auto update
@@ -63,5 +91,6 @@ IF EXIST "%ProgramFiles%\Notepad++\updater" (
 )
 
 
+:END
 ECHO END %date%-%time%
-EXIT
+EXIT %RETURNCODE%
