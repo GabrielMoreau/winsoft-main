923bf223ea84d81e245564f75de19c2b922bff3d
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-27 15:09:02 +0100

    Add deploy error code and log

--- winmerge/install.bat
+++ winmerge/install.bat
@@ -15,29 +15,54 @@ EXIT /B
 
 :INSTALL
 
-@ECHO BEGIN %date%-%time%
-
+@ECHO [INFO] BEGIN %date%-%time%
 
 SET softversion=__VERSION__
 
 
-@ECHO Silent install %softname%
+@ECHO [INFO] Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+@ECHO [INFO] Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+@ECHO [INFO] Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
+
+
+@ECHO [INFO] Execute pre-install script
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+@ECHO [INFO] Silent install %softname%
 ScriptRunner.exe -appvscript WinMerge-%softversion%-x64-Setup.exe /VERYSILENT /SP- /NORESTART /SUPPRESSMSGBOXES /FORCECLOSEAPPLICATIONS /NOCANCEL /LOG="%logdir%\%softname%-MSI.log" -appvscriptrunnerparameters -wait -timeout=300 > "%logdir%\%softname%-SR.log" 2>&1
-set SR_EXITCODE=%ERRORLEVEL%
+IF %RETURNCODE% EQU 0 set RETURNCODE=%ERRORLEVEL%
 
 FINDSTR /C:"Terminating process on timeout." "%logdir%\%softname%-SR.log" > NUL
 IF %ERRORLEVEL% EQU 0 (
   @ECHO [ERROR] Timeout detected while running ScriptRunner
-  @ECHO END %date%-%time%
-  EXIT 44
+  set RETURNCODE=140
 )
 
 
-@ECHO END %date%-%time%
+:POSTINSTALL
+@ECHO [INFO] Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
-IF %SR_EXITCODE% EQU 259 (
+IF %RETURNCODE% EQU 259 (
   @ECHO [INFO] 0 or 259 are good exit code for %softname% installer!
-  EXIT 0
+  set RETURNCODE=0
 )
-EXIT %SR_EXITCODE%
+
+:END
+@ECHO [INFO] END %date%-%time%
+EXIT %RETURNCODE%
