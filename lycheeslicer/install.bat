dba59c4aeaea3513c103cc71e824af9de3012290
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-30 08:47:38 +0100

    Add deploy error code and better log

--- lycheeslicer/install.bat
+++ lycheeslicer/install.bat
@@ -33,11 +33,23 @@ SET RETURNCODE=0
 
 
 @ECHO [INFO] Execute pre-install script
-%pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 @ECHO [INFO] Silent install %softname%
 ScriptRunner.exe -appvscript LycheeSlicer-%softversion%.exe /allusers /S -appvscriptrunnerparameters -wait -timeout=300
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:POSTINSTALL
+@ECHO [INFO] Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 @ECHO [INFO] Remove desktop shortcut
@@ -45,5 +57,6 @@ IF EXIST "%PUBLIC%\Desktop\LycheeSlicer.lnk"          DEL /F /Q "%PUBLIC%\Deskto
 IF EXIST "%ALLUSERSPROFILE%\Desktop\LycheeSlicer.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\LycheeSlicer.lnk"
 
 
+:END
 @ECHO [END] %date%-%time%
-EXIT
+EXIT %RETURNCODE%
