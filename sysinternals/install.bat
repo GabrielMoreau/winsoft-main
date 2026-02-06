0dc399a09f58291e28515251b737b4a2748ff589
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-02-06 12:24:53 +0100

    /bin/bash: ligne 1: q : commande introuvable

--- sysinternals/install.bat
+++ sysinternals/install.bat
@@ -21,12 +21,31 @@ SET softversion=__VERSION__
 SET regkey=Sysinternals
 
 
+@ECHO [INFO] Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+@ECHO [INFO] Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+@ECHO [INFO] Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
+
+
+@ECHO [INFO] Execute pre-install script
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
 @ECHO [INFO] Clean old version before install
 CALL .\uninstall.bat
 
 
 @ECHO [INFO] Silent install %softname%
 MOVE "SysinternalsSuite" "%ProgramFiles%\%regkey%"
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
 
 @ECHO [INFO] Copy uninstall script
 COPY /A /Y "uninstall.bat" "%ProgramFiles%\%regkey%\uninstall.bat"
@@ -49,6 +68,16 @@ COPY /A /Y "uninstall.bat" "%ProgramFiles%\%regkey%\uninstall.bat"
 regedit.exe /S "tmp_install.reg"
 
 
-:End
+:POSTINSTALL
+@ECHO [INFO] Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:END
 @ECHO [END] %date%-%time%
-EXIT
+EXIT %RETURNCODE%
