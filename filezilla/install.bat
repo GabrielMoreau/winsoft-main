e366c6c4ae3a38efd91a766fbba560f967562960
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-02-13 13:04:20 +0100

    Update to new deploy error code and logs

--- filezilla/install.bat
+++ filezilla/install.bat
@@ -21,20 +21,6 @@ REM Version parameter (auto update by Makefile)
 SET softversion=__VERSION__
 SET softexe=FileZilla_%softversion%_win64-setup.exe
 
-@ECHO [INFO] Uninstall previous version if exist
-IF EXIST "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" (
-  ScriptRunner.exe -appvscript "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" /S -appvscriptrunnerparameters -wait -timeout=300
-)
-IF EXIST "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" (
-  ScriptRunner.exe -appvscript "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" /S -appvscriptrunnerparameters -wait -timeout=300
-)
-IF EXIST "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" (
-  ScriptRunner.exe -appvscript "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" /S -appvscriptrunnerparameters -wait -timeout=300
-)
-IF EXIST "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" (
-  ScriptRunner.exe -appvscript "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" /S -appvscriptrunnerparameters -wait -timeout=300
-)
-
 
 @ECHO [INFO] Search PowerShell
 SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
@@ -45,11 +31,36 @@ IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%W
 
 @ECHO [INFO] Unblock PowerShell Script
 %pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
+
+
+@ECHO [INFO] Execute pre-install script
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+@ECHO [INFO] Uninstall previous version if exist
+IF EXIST "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" (
+  ScriptRunner.exe -appvscript "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" /S -appvscriptrunnerparameters -wait -timeout=300
+  IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+)
+IF EXIST "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" (
+  ScriptRunner.exe -appvscript "%ProgramFiles%\FileZilla FTP Client\uninstall.exe" /S -appvscriptrunnerparameters -wait -timeout=300
+  IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+)
+IF EXIST "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" (
+  ScriptRunner.exe -appvscript "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" /S -appvscriptrunnerparameters -wait -timeout=300
+  IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+)
+IF EXIST "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" (
+  ScriptRunner.exe -appvscript "%ProgramFiles(x86)%\FileZilla FTP Client\uninstall.exe" /S -appvscriptrunnerparameters -wait -timeout=300
+  IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+)
 
 
 @ECHO [INFO] Silent install %softname%
 ScriptRunner.exe -appvscript "%softexe%" /user=all /S -appvscriptrunnerparameters -wait -timeout=300
-SET RETURNCODE=%ERRORLEVEL%
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 @ECHO [INFO] Disable welcome and check update
@@ -60,9 +71,14 @@ IF EXIST "%ProgramFiles(x86)%\FileZilla FTP Client" (
   COPY /A /Y "fzdefaults.xml" "%ProgramFiles(x86)%\FileZilla FTP Client\fzdefaults.xml"
 )
 
+
 :POSTINSTALL
 @ECHO [INFO] Execute post-install script
-%pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
 IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
--- filezilla/pre-install.ps1
+++ filezilla/pre-install.ps1
@@ -1,6 +1,6 @@
 
 $TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-Write-Output ("`nBegin Post-Install [$TimeStamp]`n" + "=" * 40 + "`n")
+Write-Output ("Begin Pre-Install [$TimeStamp]`n" + "=" * 39 + "`n")
 
 ########################################################################
 
@@ -71,45 +71,10 @@ Write-Output "Config:`n * Version: $RefVersion`n * RegexSearch: $RefName"
 ########################################################################
 # Put your specific code here
 
-# Clean old duplicate key with Firefox in the name (same uninstall string)
-$RefUninstallString = ''
-ForEach ($Key in Get-ChildItem -Recurse $UninstallKeys) {
-	$App = Get-ItemProperty -Path $Key.PSPath
-	$DisplayName = $App.DisplayName
-	If (!($DisplayName -match $RefName)) { Continue }
-	$DisplayVersion = ToVersion $App.DisplayVersion
-	$Exe = $App.UninstallString
-	$KeyPath = $App.PSPath
-	If ($DisplayVersion -eq $RefVersion) {
-		$RefUninstallString = $Exe
-		Write-Output "Ref Key: $DisplayName / $DisplayVersion / $Exe / $KeyPath"
-	} Else {
-		Write-Output "Other Key: $DisplayName / $DisplayVersion / $Exe / $KeyPath"
-	}
-}
-
-If ($RefUninstallString -ne '') {
-	ForEach ($Key in Get-ChildItem -Recurse $UninstallKeys) {
-		$App = Get-ItemProperty -Path $Key.PSPath
-		$DisplayName = $App.DisplayName
-		If (!($DisplayName -match $RefName)) { Continue }
-		$DisplayVersion = ToVersion $App.DisplayVersion
-		$Exe = $App.UninstallString
-		$KeyPath = $App.PSPath
-		# Echo "Check Key $DisplayName : $DisplayVersion < $RefVersion ?"
-		If ((($Exe -eq $RefUninstallString) -Or ($Exe -eq $Null)) -And ($DisplayVersion -lt $RefVersion)) {
-			Write-Output "Remove Key: $DisplayName / $DisplayVersion / $Exe / $KeyPath"
-			Remove-Item -Path "$KeyPath" -Force -Recurse -ErrorAction SilentlyContinue
-		} Else {
-			Write-Output "Keep Key: $DisplayName / $DisplayVersion / $Exe / $KeyPath / $DisplayVersion"
-		}
-	}
-}
-
 ########################################################################
 
 # View
-$ReturnCode = 143
+$ReturnCode = 0
 ForEach ($Key in Get-ChildItem -Recurse $UninstallKeys) {
 	$App = Get-ItemProperty -Path $Key.PSPath
 	If ($App.DisplayName -notmatch $RefName) { Continue }
@@ -117,14 +82,6 @@ ForEach ($Key in Get-ChildItem -Recurse $UninstallKeys) {
 	$DisplayVersion = ToVersion $App.DisplayVersion
 	$KeyProduct     = $Key.PSChildName
 	Write-Output "Installed: $($App.DisplayName) / $DisplayVersion / $KeyProduct / $($App.UninstallString)"
-
-	If ($DisplayVersion -gt $RefVersion) {
-		$ReturnCode = [Math]::Min($ReturnCode, 141)
-	} ElseIf ($DisplayVersion -eq $RefVersion) {
-		$ReturnCode = 0
-	} Else {
-		$ReturnCode = [Math]::Min($ReturnCode, 142)
-	}
 }
 Write-Output "ReturnCode: $ReturnCode"
 Exit $ReturnCode
