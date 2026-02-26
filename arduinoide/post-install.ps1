5e840412322a8ff4a06aa8f5ae42932fe1f64894
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-02-26 10:57:58 +0100

    Add new deploy error codes and logs

--- arduinoide/post-install.ps1
+++ arduinoide/post-install.ps1
@@ -1,6 +1,6 @@
 
 $TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-Write-Output ("Begin Pre-Install [$TimeStamp]`n" + "=" * 39 + "`n")
+Write-Output ("`nBegin Post-Install [$TimeStamp]`n" + "=" * 40 + "`n")
 
 ########################################################################
 
@@ -71,16 +71,10 @@ Write-Output "Config:`n * Version: $RefVersion`n * RegexSearch: $RefName"
 ########################################################################
 # Put your specific code here
 
-Write-Output "Info: import certificate Adafruit"
-Import-Certificate -FilePath 'Adafruit.cer' -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
-
-Write-Output "Info: import certificate Arduino"
-Import-Certificate -FilePath 'Arduino.cer'  -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
-
 ########################################################################
 
 # View
-$ReturnCode = 0
+$ReturnCode = 143
 ForEach ($Key in Get-ChildItem -Recurse $UninstallKeys) {
 	$App = Get-ItemProperty -Path $Key.PSPath
 	If ($App.DisplayName -notmatch $RefName) { Continue }
@@ -88,6 +82,14 @@ ForEach ($Key in Get-ChildItem -Recurse $UninstallKeys) {
 	$DisplayVersion = ToVersion $App.DisplayVersion
 	$KeyProduct     = $Key.PSChildName
 	Write-Output "Installed: $($App.DisplayName) / $DisplayVersion / $KeyProduct / $($App.UninstallString)"
+
+	If ($DisplayVersion -gt $RefVersion) {
+		$ReturnCode = [Math]::Min($ReturnCode, 141)
+	} ElseIf ($DisplayVersion -eq $RefVersion) {
+		$ReturnCode = 0
+	} Else {
+		$ReturnCode = [Math]::Min($ReturnCode, 142)
+	}
 }
 Write-Output "ReturnCode: $ReturnCode"
 Exit $ReturnCode
