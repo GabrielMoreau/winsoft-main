682c3925253c6a24c2ad4547f2dc47caede38c7c
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-03-04 10:06:08 +0100

    Add support for deploy error code and logs

--- onlyoffice/install.bat
+++ onlyoffice/install.bat
@@ -17,12 +17,39 @@ EXIT /B
 
 @ECHO [BEGIN] %date%-%time%
 
-
 SET softversion=__VERSION__
+
+
+@ECHO [INFO] Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+@ECHO [INFO] Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+@ECHO [INFO] Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
+
+
+@ECHO [INFO] Execute pre-install script
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 @ECHO [INFO] Silent install %softname%
 ScriptRunner.exe -appvscript MsiExec.exe /i "DesktopEditors-%softversion%-x64.msi" /qn /norestart /L*v "%logdir%\%softname%-MSI.log" -appvscriptrunnerparameters -wait -timeout=300
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:POSTINSTALL
+@ECHO [INFO] Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 REM @ECHO [INFO] Remove desktop shortcut
@@ -30,5 +57,6 @@ REM IF EXIST "%PUBLIC%\Desktop\OnlyOffice.lnk"          DEL /F /Q "%PUBLIC%\Desk
 REM IF EXIST "%ALLUSERSPROFILE%\Desktop\OnlyOffice.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\OnlyOffice.lnk"
 
 
+:END
 @ECHO [END] %date%-%time%
-EXIT
+EXIT %RETURNCODE%
