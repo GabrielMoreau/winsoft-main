3c133d63ae7fb110d70d83dc8dcd6acbcfde801b
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-03-11 12:26:49 +0100

    Add deploy error code support and logs

--- hpsupportassist/install.bat
+++ hpsupportassist/install.bat
@@ -20,6 +20,23 @@ EXIT /B
 SET softversion=__VERSION__
 
 
+@ECHO [INFO] Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+@ECHO [INFO] Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+@ECHO [INFO] Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
+
+
+@ECHO [INFO] Execute pre-install script
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
 @ECHO [INFO] Disabled automatic install, notifytray and notifypopup
 REG ADD "HKLM\SOFTWARE\WOW6432Node\Hewlett-Packard\HPActiveSupport\HPSF" /v NotifyTray /t REG_SZ /d 0 /f
 REG ADD "HKLM\SOFTWARE\WOW6432Node\Hewlett-Packard\HPActiveSupport\HPSF" /v NotifyPopup /t REG_SZ /d 0 /f
@@ -28,6 +45,17 @@ REG ADD "HKLM\SOFTWARE\WOW6432Node\Hewlett-Packard\HPActiveSupport\HPHC" /v Inst
 
 @ECHO [INFO] Silent install %softname%
 ScriptRunner.exe -appvscript InstallHPSA.exe /S /v/qn -appvscriptrunnerparameters -wait -timeout=1200
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:POSTINSTALL
+@ECHO [INFO] Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 @ECHO [INFO] Remove desktop shortcut
@@ -35,5 +63,6 @@ IF EXIST "%PUBLIC%\Desktop\HP*Support*Assistant.lnk"          DEL /F /Q "%PUBLIC
 IF EXIST "%ALLUSERSPROFILE%\Desktop\HP*Support*Assistant.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\HP*Support*Assistant.lnk"
 
 
+:END
 @ECHO [END] %date%-%time%
-EXIT
+EXIT %RETURNCODE%
