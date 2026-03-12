d6039d529bfcd733dd6b7109e9fe453e9256182f
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-03-12 10:23:09 +0100

    Add deploy error code support and logs

--- kopia/install.bat
+++ kopia/install.bat
@@ -20,12 +20,40 @@ EXIT /B
 SET softversion=__VERSION__
 
 
+@ECHO [INFO] Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+@ECHO [INFO] Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+@ECHO [INFO] Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
+
+
+@ECHO [INFO] Execute pre-install script
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
 @ECHO [INFO] Kill running process
 TASKKILL /T /F /IM KopiaUI.exe /IM Kopia.exe || VER >NUL
 
 
 @ECHO [INFO] Silent install %softname%
 ScriptRunner.exe -appvscript KopiaUI-Setup-%softversion%.exe /S /allusers /disableAutoUpdates -appvscriptrunnerparameters -wait -timeout=300
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:POSTINSTALL
+@ECHO [INFO] Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
 
 
 @ECHO [INFO] Remove desktop shortcut
@@ -33,5 +61,6 @@ IF EXIST "%PUBLIC%\Desktop\KopiaUI.lnk"          DEL /F /Q "%PUBLIC%\Desktop\Kop
 IF EXIST "%ALLUSERSPROFILE%\Desktop\KopiaUI.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\KopiaUI.lnk"
 
 
+:END
 @ECHO [END] %date%-%time%
-EXIT
+EXIT %RETURNCODE%
