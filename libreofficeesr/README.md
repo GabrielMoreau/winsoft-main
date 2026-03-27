93f797c6281c9f0cf3ca22048bea561d058716bc
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-03-27 17:14:15 +0100

    Add LibreOffice Previous Stable Package

---
+++ libreofficeesr/README.md
@@ -0,0 +1,19 @@
+# LibreOffice Previous Version (ESR) - Libre Office suite based on the OpenDocument standard
+
+LibreOffice is a free and open-source office productivity software suite.
+LibreOffice uses the OpenDocument standard as its native file format.
+
+* Website : https://www.libreoffice.org/
+* Wikipedia : https://en.wikipedia.org/wiki/LibreOffice
+
+
+## Register Key
+
+Example :
+
+ | Hive | DisplayName | Publisher | DisplayVersion | KeyProduct | UninstallExe |
+ |:---- |:----------- |:--------- |:-------------- |:---------- |:------------ |
+ | HKLM | LibreOffice 25.2 Help Pack (French) | The Document Foundation | 25.2.3.2 | `{7F30DF70-0CDD-4CF6-AD99-63C52550D9CE}` | `MsiExec.exe /I{7F30DF70-0CDD-4CF6-AD99-63C52550D9CE}` |
+ | HKLM | LibreOffice 25.2.3.2                | The Document Foundation | 25.2.3.2 | `{55CF6D12-B29A-4610-9E4A-1ACFE722B691}` | `MsiExec.exe /I{55CF6D12-B29A-4610-9E4A-1ACFE722B691}` |
+ | HKLM | LibreOffice 26.2 Help Pack (French) | The Document Foundation | 26.2.0.3 | `{69D36388-2AA9-4AD5-A5D0-F1F6A7DF72C8}` | `MsiExec.exe /I{69D36388-2AA9-4AD5-A5D0-F1F6A7DF72C8}` |
+ | HKLM | LibreOffice 26.2.0.3                | The Document Foundation | 26.2.0.3 | `{5B9B7FC3-E7A6-4B71-922B-BDAB5FD55147}` | `MsiExec.exe /I{5B9B7FC3-E7A6-4B71-922B-BDAB5FD55147}` |
---
+++ libreofficeesr/install.bat
@@ -0,0 +1,101 @@
+
+REM
+REM   LibreOfficeESR
+REM
+
+REM Name
+SET softname=LibreOfficeESR
+
+SET logdir=__LOGDIR__
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+@ECHO [BEGIN] %date%-%time%
+
+
+SET softversion=__VERSION__
+SET process=soffice.bin
+SET MAX_RETRY=1
+
+
+@ECHO [INFO] Search PowerShell
+SET pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe
+
+@ECHO [INFO] Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+@ECHO [INFO] Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+SET RETURNCODE=0
+
+
+@ECHO [INFO] Execute pre-install script
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:REINSTALL
+@ECHO [INFO] Kill running process
+TASKKILL /T /F /IM soffice.bin /IM soffice.exe /IM swriter.exe /IM scalc.exe /IM simpress.exe /IM sdraw.exe /IM smath.exe /IM sbase.exe || VER >NUL
+
+@ECHO [INFO] Silent Install %softname%
+REM https://wiki.documentfoundation.org/Deployment_and_Migration
+IF EXIST "LibreOffice_%softversion%_Win_x86-64.msi" (
+  ScriptRunner.exe -appvscript MsiExec.exe /i "LibreOffice_%softversion%_Win_x86-64.msi" /qn /norestart /l "%logdir%\%softname%-MSI.log" CREATEDESKTOPLINK=0 RebootYesNo=No ISCHECKFORPRODUCTUPDATES=0 REGISTER_NO_MSO_TYPES=1 REMOVE=gm_o_Onlineupdate SELECT_WORD=0 SELECT_EXCEL=0 SELECT_POWERPOINT=0 USERNAME="LEGI" ADDLOCAL=ALL -appvscriptrunnerparameters -wait -timeout=450
+  IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+) ELSE (
+  @ECHO [ERROR] Installer is not in the archive!
+  IF %RETURNCODE% EQU 0 SET RETURNCODE=149
+)
+
+@ECHO [INFO] Check RETURNCODE
+IF %RETURNCODE% NE 0 (
+  IF %MAX_RETRY% NE 0 (
+    @ECHO [WARN] Try installation again
+    SET /A MAX_RETRY-=1
+    SET RETURNCODE=0
+    GOTO REINSTALL
+  )
+)
+
+@ECHO [INFO] Check if installed
+IF EXIST "%ProgramFiles%\LibreOffice\program\soffice.exe" (
+  GOTO INSTALLHELP
+) ELSE (
+  IF "%MAX_RETRY%"=="0" (
+    @ECHO [ERROR] MAX_RETRY installation done and no %softname%
+    IF %RETURNCODE% EQU 0 SET RETURNCODE=140
+    GOTO POSTINSTALL
+  ) ELSE (
+    @ECHO [WARN] Try installation again
+    SET /A MAX_RETRY-=1
+    GOTO REINSTALL
+  )
+)
+
+
+:INSTALLHELP
+@ECHO [INFO] Silent help install
+ScriptRunner.exe -appvscript MsiExec.exe /i "LibreOffice_%softversion%_Win_x86-64_helppack_fr.msi" /qn /norestart /l "%logdir%\%softname%-HELP.log" -appvscriptrunnerparameters -wait -timeout=300
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:POSTINSTALL
+@ECHO [INFO] Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF %RETURNCODE% EQU 0 SET RETURNCODE=%ERRORLEVEL%
+
+
+:END
+@ECHO [END] %date%-%time%
+EXIT %RETURNCODE%
