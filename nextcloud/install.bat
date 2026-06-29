ba06039a4c4a628674c49f72d61b625f510d196c
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-06-29 15:08:07 +0200

    Add support for QEXEADMIN

--- nextcloud/install.bat
+++ nextcloud/install.bat
@@ -19,6 +19,8 @@ EXIT /B
 
 SET "softversion=__VERSION__"
 SET "process=nextcloud.exe"
+SET "qexeadmin=__QEXEADMIN__"
+SET "mainexe=%ProgramFiles%\Nextcloud\nextcloud.exe"
 
 @ECHO [INFO] Search PowerShell
 SET "pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe"
@@ -30,6 +32,17 @@ IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET "pwrsh=%
 @ECHO [INFO] Unblock PowerShell Script
 %pwrsh% "Unblock-File -Path .\*.ps1"
 SET "RETURNCODE=0"
+
+
+:QEXEADMRESET
+IF "%qexeadmin%"=="false" (
+  FOR %%F in ("%mainexe:;=" "%") do (
+    IF EXIST "%%~F" (
+      @ECHO [INFO] Reset ACL on %%~F
+      icacls "%%~F" /reset || VER >NUL
+    )
+  )
+)
 
 
 @ECHO [INFO] Execute pre-install script
@@ -54,6 +67,17 @@ IF EXIST ".\pre-install.ps1" (
   IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
 )
 IF "%RETURNCODE%"=="0" SET "RETURNCODE=%ERRORLEVEL%"
+
+
+:QEXEADMDENY
+IF "%qexeadmin%"=="false" (
+  FOR %%F in ("%mainexe:;=" "%") do (
+    IF EXIST "%%~F" (
+      @ECHO [INFO] Restrict ACL for admin on %%~F
+      icacls "%%~F" /deny "*S-1-5-32-544:(X)" || VER >NUL
+    )
+  )
+)
 
 
 :END
