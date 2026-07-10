a6b69bc453d99888a02b29f8c462fa35a4920e53
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-07-10 18:40:36 +0200

    Add PDF4DT software

---
+++ pdf4qt/README.md
@@ -0,0 +1,18 @@
+# PDF4QT - PDF editor
+
+PDF4QT is a free, open-source open-source PDF editor designed for both Windows and Linux.
+This modern solution offers an unparalleled experience for viewing, editing, and rendering PDF documents.
+
+* Website : https://jakubmelka.github.io/
+
+* Download : https://github.com/JakubMelka/PDF4QT/releases
+* Forge : https://github.com/JakubMelka/PDF4QT
+
+
+## Register Key
+
+Example :
+
+ | Hive | DisplayName | Publisher | DisplayVersion | KeyProduct | UninstallExe |
+ |:---- |:----------- |:--------- |:-------------- |:---------- |:------------ |
+ | HKLM | PDF4QT | Jakub Melka | 1.6.0.0 | `{74C4430D-3936-4DB4-8E55-14DBBF8A5DA0}` | `MsiExec.exe /I{74C4430D-3936-4DB4-8E55-14DBBF8A5DA0}` |
---
+++ pdf4qt/install.bat
@@ -0,0 +1,89 @@
+
+REM
+REM   PDF4QT
+REM
+
+REM Name
+SET "softname=PDF4QT"
+
+SET "logdir=__LOGDIR__"
+IF NOT EXIST "%logdir%" (
+  MKDIR "%logdir%"
+)
+CALL :INSTALL 1> "%logdir%\%softname%.log" 2>&1
+EXIT /B
+
+:INSTALL
+
+@ECHO [BEGIN] %date%-%time%
+
+SET "softversion=__VERSION__"
+SET "qexeadmin=__QEXEADMIN__"
+SET "mainexe=%ProgramFiles(x86)%\PDF4QT\Pdf4QtDiff.exe"
+SET "mainexe=%mainexe%;%ProgramFiles(x86)%\PDF4QT\Pdf4QtEditor.exe"
+SET "mainexe=%mainexe%;%ProgramFiles(x86)%\PDF4QT\Pdf4QtPageMaster.exe"
+SET "mainexe=%mainexe%;%ProgramFiles(x86)%\PDF4QT\Pdf4QtViewer.exe"
+
+
+@ECHO [INFO] Search PowerShell
+SET "pwrsh=%WINDIR%\System32\WindowsPowerShell\V1.0\powershell.exe"
+IF EXIST "%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe" SET "pwrsh=%WINDIR%\Sysnative\WindowsPowerShell\V1.0\powershell.exe"
+
+@ECHO [INFO] Add rights
+%pwrsh% Set-ExecutionPolicy RemoteSigned -Force -Scope LocalMachine
+
+@ECHO [INFO] Unblock PowerShell Script
+%pwrsh% "Unblock-File -Path .\*.ps1"
+SET "RETURNCODE=0"
+
+
+:QEXEADMRESET
+IF "%qexeadmin%"=="false" (
+  FOR %%F in ("%mainexe:;=" "%") do (
+    IF EXIST "%%~F" (
+      @ECHO [INFO] Reset ACL on %%~F
+      icacls "%%~F" /reset || VER >NUL
+    )
+  )
+)
+
+
+@ECHO [INFO] Execute pre-install script
+IF EXIST ".\pre-install.ps1" %pwrsh% -File ".\pre-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+IF "%RETURNCODE%"=="0" SET "RETURNCODE=%ERRORLEVEL%"
+
+
+@ECHO [INFO] Silent install %softname%
+ScriptRunner.exe -appvscript MsiExec.exe /i "JakubMelka.PDF4QT_%softversion%.msi" /qn /norestart /L*v "%logdir%\%softname%-MSI.log" -appvscriptrunnerparameters -wait -timeout=300
+IF "%RETURNCODE%"=="0" SET "RETURNCODE=%ERRORLEVEL%"
+
+
+:POSTINSTALL
+@ECHO [INFO] Execute post-install script
+IF EXIST ".\pre-install.ps1" (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1>> "%logdir%\%softname%-PS1.log" 2>&1
+) ELSE (
+  IF EXIST ".\post-install.ps1" %pwrsh% -File ".\post-install.ps1" 1> "%logdir%\%softname%-PS1.log" 2>&1
+)
+IF "%RETURNCODE%"=="0" SET "RETURNCODE=%ERRORLEVEL%"
+
+
+@ECHO [INFO] Remove desktop shortcut
+IF EXIST "%PUBLIC%\Desktop\%softname%.lnk"          DEL /F /Q "%PUBLIC%\Desktop\%softname%.lnk"
+IF EXIST "%ALLUSERSPROFILE%\Desktop\%softname%.lnk" DEL /F /Q "%ALLUSERSPROFILE%\Desktop\%softname%.lnk"
+
+
+:QEXEADMDENY
+IF "%qexeadmin%"=="false" (
+  FOR %%F in ("%mainexe:;=" "%") do (
+    IF EXIST "%%~F" (
+      @ECHO [INFO] Restrict ACL for admin on %%~F
+      icacls "%%~F" /deny "*S-1-5-32-544:(X)" || VER >NUL
+    )
+  )
+)
+
+
+:END
+@ECHO [END] %date%-%time%
+EXIT %RETURNCODE%
